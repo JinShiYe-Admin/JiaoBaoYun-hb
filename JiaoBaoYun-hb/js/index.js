db7381c09e548d6b6098a9d92e7daf759ae5577d
rockan007 <rockan007@gmail.com>
2016-10-24 11:32:57 +0800

    修改状态栏

--- JiaoBaoYun-hb/js/index.js
+++ JiaoBaoYun-hb/js/index.js
@@ -10,7 +10,7 @@ mui.plusReady(function() {
 	Statusbar.barHeight(); //设置距离顶部的高度
 	var header = document.querySelector(".mui-bar-nav"); //顶部导航
 	//设置顶部导航高度（状态栏）
-	header.style.height = localStorage.getItem('$Statusbar');
+	header.style.height = localStorage.getItem('StatusHeightNo')+45+'px';
 
 	//设置默认打开首页显示的子页序号；
 	var Index = 0;
@@ -18,7 +18,7 @@ mui.plusReady(function() {
 	var subpages = ['zone_sub.html', 'tab_knowledge.html', 'clip/clip_sub.html', 'cloud/cloud_sub.html'];
 
 	var subpage_style = {
-		top: localStorage.getItem('$Statusbar'), //设置距离顶部的距离
+		top: (localStorage.getItem('StatusHeightNo')+45)+'px', //设置距离顶部的距离
 		bottom: '51px'
 	};
 
