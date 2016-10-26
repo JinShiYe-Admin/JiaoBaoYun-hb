d7ec1a4258db5e311b8a38a2886079f2bf80fe5b
rockan007 <rockan007@gmail.com>
2016-10-26 17:36:31 +0800

    提交

--- JiaoBaoYun-hb/js/utils/carouselControl.js
+++ JiaoBaoYun-hb/js/utils/carouselControl.js
@@ -63,19 +63,35 @@ var  carousel=(function(mod){
 			        +'<p>'+item.word+'</p>'
 			      +'</a>';
 	}
+	/**
+   * 加载底部条状物
+   * @param {Object} imgUrls
+   */
+	mod.addStrip=function(imgUrls){
+		var strip=document.body.querySelector(".mui-slider-indicator,.mui-text-right");
+		for(i=0;i<imgUrls.length;i++){
+			var div=document.createElement('div');
+			if(i==0){
+				div.className="mui-indicator mui-active";
+			}else{
+				div.className="mui-indicator";
+			}
+			strip.appendChild(div);
+		}
+	}
 	return mod;
 })(window.carousel||{})
-var addImg=function(imgUrls,titles){
-	var group=document.body.querySelector(".mui-slider-group,.mui-slider-loop")
-	addDiv(imgUrls[imgUrls.length-1],titles[titles.length-1],group)
-	imgUrls.forEach(function(imgUrl,index,imgUrls){
-		var div=document.createElement('div');
-		div.className="mui-slider-item"
-		div.innerHTML=createImgInner(imgUrl,titles[index])
-		group.appendChild(div);
-	})
-	addDiv(imgUrls[0],titles[0],group)
-}
+//var addImg=function(imgUrls,titles){
+//	var group=document.body.querySelector(".mui-slider-group,.mui-slider-loop")
+//	addDiv(imgUrls[imgUrls.length-1],titles[titles.length-1],group)
+//	imgUrls.forEach(function(imgUrl,index,imgUrls){
+//		var div=document.createElement('div');
+//		div.className="mui-slider-item"
+//		div.innerHTML=createImgInner(imgUrl,titles[index])
+//		group.appendChild(div);
+//	})
+//	addDiv(imgUrls[0],titles[0],group)
+//}
 //var createImgInner=function(imgUrl,title){
 //	var innerHTML='<a href="#">'
 //		        +'<img src="'+imgUrl+'">'
@@ -83,22 +99,7 @@ var addImg=function(imgUrls,titles){
 //		      +'</a>';
 //	return innerHTML;		      
 //}
-///**
-// * 加载底部条状物
-// * @param {Object} imgUrls
-// */
-//var addStrip=function(imgUrls){
-//	var strip=document.body.querySelector(".mui-slider-indicator,.mui-text-right");
-//	for(i=0;i<imgUrls.length;i++){
-//		var div=document.createElement('div');
-//		if(i==0){
-//			div.className="mui-indicator mui-active"
-//		}else{
-//			div.className="mui-indicator"
-//		}
-//		strip.appendChild(div);
-//	}
-//}
+
 ///**
 // * 加载第一条和最后一条数据
 // * @param {Object} imgUrl
