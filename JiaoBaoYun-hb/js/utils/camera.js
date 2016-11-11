12c7426d43b7705a9a0d61652af723fb4553e9d5
rockan007 <rockan007@gmail.com>
2016-11-11 17:23:31 +0800

    发布作业界面 1.录音功能 2.拍摄功能 3.拍照功能 作业评论界面

--- JiaoBaoYun-hb/js/utils/camera.js
+++ JiaoBaoYun-hb/js/utils/camera.js
@@ -2,34 +2,47 @@
  * 相机模块
  */
 var camera=(function(mod){
-	mod.imgPaths=[];
+	mod.filePaths=[];
 	//获取相机
 	mod.getCamera=function(){
 		var cmr=plus.camera.getCamera(1);
 		return cmr;
 	}
-	//获取图片
+	//拍照
 	mod.getPic=function(cmr,managePic){
 		var res = cmr.supportedImageResolutions[0];
 		var fmt = cmr.supportedImageFormats[0];
 		cmr.captureImage(function(path){
 			console.log( "Capture image success: " + path );
 			//数组添加path
-			mod.imgPaths.push(path);
+			mod.filePaths.push(path);
 			//处理图片
-			managePic(mod.getPicPath(path));
+			managePic(mod.getAbsolutePath(path));
 		},
 		function(err){
 			console.log( "Capture image failed: " + err.message );  
 		},
 		{format:fmt})
 	}
-	//获取图片路径
-	mod.getPicPath=function(path){
-	 var picPath=plus.io.convertLocalFileSystemURL(path);
-	 picPath="file://"+picPath;
-	 console.log('picture path='+picPath);
-	 return picPath;
+	//录像
+	mod.getVideo=function(cmr,manageVideo){
+		var res=cmr.supportedVideoResolutions[0];
+		var fmt=cmr.supportedVideoFormats[0];
+		cmr.startVideoCapture(function(path){
+			mod.filePaths.push(path);
+			manageVideo(mod.getAbsolutePath(path));
+		},
+		function(error){
+			mui.toast('发生错误，请重试')
+		},
+		{format:fmt})
+	}
+	//获取局对路径
+	mod.getAbsolutePath=function(path){
+	 var abPath=plus.io.convertLocalFileSystemURL(path);
+	 abPath="file://"+abPath;
+	 console.log('file path='+abPath);
+	 return abPath;
 	}
 	return mod;
-})(window.camera||{})
+})(window.camera||{});
