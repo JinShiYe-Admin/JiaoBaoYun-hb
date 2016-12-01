c2043f6b1bfc6b425c45ff2b9cf693ee7fae5ed2
rockan007 <rockan007@gmail.com>
2016-12-01 12:52:10 +0800

    群成员界面和群成员资料界面
    更改群成员退群按钮显示状态；
    点击成员资料空间，跳转至个人空间

---
+++ JiaoBaoYun-hb/js/quan/group-pInfo.js
@@ -0,0 +1,47 @@
+mui.init()
+var pInfo; //{"gid":1,"gutid":9,"utid":5,
+//"ugname":"BugHunter","ugnick":"BugHunter",
+//"uimg":"http://oh2zmummr.bkt.clouddn.com/headimge5.png","mstype":3}
+var accountInfo;
+mui.plusReady(function() {
+		events.preload('zonep_sub.html',100);
+		window.addEventListener('postPInfo', function(e) {
+			pInfo = e.detail.data;
+			console.log('獲取的個人信息：' + JSON.stringify(pInfo));
+			getAccountInfo(manageAccountInfo);
+		})
+		
+	})
+	/**
+	 * 獲取個人賬號信息
+	 */
+var getAccountInfo = function(callback) {
+	var wd = plus.nativeUI.showWaiting(storageKeyName.WAITING);
+	postDataPro_PostUinf({
+		vvl: pInfo.utid,
+		vtp: 'p'
+	}, wd, function(data) {
+		wd.close();
+		console.log('詳細資料頁面獲取的個人資料：' + JSON.stringify(data));
+		if(data.RspCode == '0000') {
+			callback(data.RspData[0]);
+		} else {
+			mui.toast(data.RspTxt);
+		}
+	})
+}
+var manageAccountInfo = function(data) {
+	accountInfo=data;
+	console.log('獲取的個人賬號信息：' + JSON.stringify(data));
+	//{"utid":5,"uid":"18853113151","uname":"test867830028690115",
+	//"unick":"BugHunter","usex":0,"utxt":null,
+	//"uimg":"http://oh2zmummr.bkt.clouddn.com/headimge5.png"}
+	document.getElementById('info-headImg').src=data.uimg;
+	document.getElementById('info-name').innerText=data.uname;
+	document.getElementById('info-nick').innerText=data.unick;
+}
+var addListener=function(){
+	events.addTap('personal-space',function(){
+		events.fireToPageWithData('zonep_sub.html','postUTID',pInfo.utid);
+	})
+}
