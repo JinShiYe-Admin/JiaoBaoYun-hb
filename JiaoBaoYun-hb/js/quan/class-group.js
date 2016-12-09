576690971b0507231e805dec26f2ec72d42c5066
rockan007 <rockan007@gmail.com>
2016-12-09 15:24:07 +0800

    0003920: 群名字太长时，班级空间显示出错

--- JiaoBaoYun-hb/js/quan/class-group.js
+++ JiaoBaoYun-hb/js/quan/class-group.js
@@ -21,12 +21,12 @@ var quit_group3 = document.getElementById('quit-group3');
 mui.plusReady(function() {
 		events.preload('group-pInfo.html', 200);
 		window.addEventListener('postGroupInfo', function(e) {
-			isMaster=false;
+			isMaster = false;
 			console.log(JSON.stringify(e.detail.data));
 			if(e.detail.data) {
 				groupId = e.detail.data.classId;
 				groupName = e.detail.data.className;
-				document.getElementById('title').innerText = groupName;
+				document.getElementById('title').innerText =getHeadText(groupName);
 				groupRoles = [];
 				allcount = 0;
 				setGride();
@@ -36,18 +36,18 @@ mui.plusReady(function() {
 				getUserInGroup(-1, function(data) {
 					groupRoles = data;
 					console.log('获取本人在群的所有信息：' + JSON.stringify(data));
-					groupRoles.forEach(function(groupRole){
-						if(groupRole==1){
-							isMaster=true;
+					groupRoles.forEach(function(groupRole) {
+						if(groupRole == 1) {
+							isMaster = true;
 						}
 					})
 				});
 			}
 		})
-		window.addEventListener('groupInfoChanged',function(){
-			setGride();
-		})
-		//群組頭像點擊事件
+		window.addEventListener('groupInfoChanged', function() {
+				setGride();
+			})
+			//群組頭像點擊事件
 		mui('#gride1').on('tap', '.mui-table-view-cell', function() {
 			events.fireToPageWithData('group-pInfo.html', 'postPInfo', this.info);
 		})
@@ -55,21 +55,21 @@ mui.plusReady(function() {
 			events.fireToPageWithData('group-pInfo.html', 'postPInfo', this.info);
 		})
 		mui('#gride3').on('tap', '.mui-table-view-cell', function() {
-			events.fireToPageWithData('group-pInfo.html', 'postPInfo', this.info);
-		})
-		//退出按鈕點擊事件
+				events.fireToPageWithData('group-pInfo.html', 'postPInfo', this.info);
+			})
+			//退出按鈕點擊事件
 		quit_group1.addEventListener('tap', function() {
 				getUserInGroup(0, showChoices);
 			})
-		//退出按鈕點擊事件
+			//退出按鈕點擊事件
 		quit_group2.addEventListener('tap', function() {
 				getUserInGroup(2, showChoices);
 			})
-		//退出按鈕點擊事件
+			//退出按鈕點擊事件
 		quit_group3.addEventListener('tap', function() {
-				getUserInGroup(3, showChoices);
-			})
-		
+			getUserInGroup(3, showChoices);
+		})
+
 	})
 	/**
 	 * 获取用户在群组中的信息
@@ -95,11 +95,17 @@ var getUserInGroup = function(mstype, callback) {
 		}
 	})
 }
-/**
- * 是否顯示退出按鈕
- * @param {Object} mstype
- * @param {Object} b
- */
+var getHeadText = function(className) {
+		if(className.length > 8) {
+			className = className.substring(0, 10) + '...'
+		}
+		return className;
+	}
+	/**
+	 * 是否顯示退出按鈕
+	 * @param {Object} mstype
+	 * @param {Object} b
+	 */
 var isShowQuit = function(mstype, b) {
 		switch(mstype) {
 			case 3:
@@ -152,9 +158,9 @@ var quitGroupAll = function() {
 		groupRoles.forEach(function(groupRole, i) {
 			if(groupRole.mstype == 1) {
 				allcount++;
-			}else{
+			} else {
 				//班主任不能退出老师身份
-				if(isMaster&&groupRole.mstype == 2){
+				if(isMaster && groupRole.mstype == 2) {
 					allcount++;
 				} else {
 					quitGroup(groupRole, allCallback);
@@ -203,126 +209,126 @@ var quitSquad = function(roleInfo) {
 	 * @param {Object} callback
 	 */
 var quitGroup = function(roleInfo, callback) {
-	var wd = plus.nativeUI.showWaiting(storageKeyName.WAITING);
-	postDataPro_PostGuD({
-		vvl: roleInfo.gutid
-	}, wd, function(data) {
-		console.log('退群返回值：' + JSON.stringify(data));
-		wd.close();
-		if(data.RspCode == '0000') {
-			mui.toast('退群成功');
-			callback(roleInfo);
-		} else {
-			mui.toast(data.RspTxt);
-		}
-	})
-}
-/**
- * 界面加載數據初始化
- */
-var setGride = function() {
-	console.log('传送的groupId:' + groupId)
-	getGroupInfo(3);
-	getGroupInfo(2);
-	getGroupInfo(0);
-	getUserInGroup(3);
-	getUserInGroup(2);
-	getUserInGroup(0);
-}
-/**
- * 不同群組加載數據
- * @param {Object} vvl 群組類型 0：家長2老師3家長
- */
-var getGroupInfo = function(vvl) {
-		var item;
-		switch(vvl) {
-			case 3:
-				item = gride3;
-				break;
-			case 2:
-				item = gride2;
-				break;
-			case 0:
-				item = gride1;
-				break;
-			default:
-				break;
-		}
-		//清空歷史數據
-		events.clearChild(item);
-		var wd = plus.nativeUI.showWaiting(storageKeyName.WAITING)
-		//請求群組成員數據
-		postDataPro_PostGusers({
-			top: -1,
-			vvl: groupId,
-			vvl1: vvl
-		}, wd, function(groupData) {
+		var wd = plus.nativeUI.showWaiting(storageKeyName.WAITING);
+		postDataPro_PostGuD({
+			vvl: roleInfo.gutid
+		}, wd, function(data) {
+			console.log('退群返回值：' + JSON.stringify(data));
 			wd.close();
-			console.log('获取群组成员：' + vvl + JSON.stringify(groupData))
-			//成功囘調
-			if(groupData.RspCode == '0000' && groupData.RspData != null) {
-//				createGride(item, data.RspData);
-				getRemarkData(groupData.RspData,function(Remarkdata){
-					var list=[];
-					if(Remarkdata.RspCode=='0000'){
-						list=addRemarkData(groupData.RspData,Remarkdata.RspData);
-					}else{
-						list=addRemarkData(groupData.RspData)
-					}
-					events.clearChild(item);
-					console.log('最终呈现的数据：'+vvl+JSON.stringify(list));
-					createGride(item, list);
-				})
-
+			if(data.RspCode == '0000') {
+				mui.toast('退群成功');
+				callback(roleInfo);
+			} else {
+				mui.toast(data.RspTxt);
 			}
-		});
+		})
 	}
-var addRemarkData=function(list,remarkList){
-	if(remarkList){
-		for(var i in list){
-			for(var j in remarkList){
-				if(list[i].utid==remarkList[j].butid){
-					list[i].bunick=remarkList[j].bunick;
-					break;
-				}else{
-					list[i].bunick=list[i].gname;
+	/**
+	 * 界面加載數據初始化
+	 */
+var setGride = function() {
+		console.log('传送的groupId:' + groupId)
+		getGroupInfo(3);
+		getGroupInfo(2);
+		getGroupInfo(0);
+		getUserInGroup(3);
+		getUserInGroup(2);
+		getUserInGroup(0);
+	}
+	/**
+	 * 不同群組加載數據
+	 * @param {Object} vvl 群組類型 0：家長2老師3家長
+	 */
+var getGroupInfo = function(vvl) {
+	var item;
+	switch(vvl) {
+		case 3:
+			item = gride3;
+			break;
+		case 2:
+			item = gride2;
+			break;
+		case 0:
+			item = gride1;
+			break;
+		default:
+			break;
+	}
+	//清空歷史數據
+	events.clearChild(item);
+	var wd = plus.nativeUI.showWaiting(storageKeyName.WAITING)
+		//請求群組成員數據
+	postDataPro_PostGusers({
+		top: -1,
+		vvl: groupId,
+		vvl1: vvl
+	}, wd, function(groupData) {
+		wd.close();
+		console.log('获取群组成员：' + vvl + JSON.stringify(groupData))
+			//成功囘調
+		if(groupData.RspCode == '0000' && groupData.RspData != null) {
+			//				createGride(item, data.RspData);
+			getRemarkData(groupData.RspData, function(Remarkdata) {
+				var list = [];
+				if(Remarkdata.RspCode == '0000') {
+					list = addRemarkData(groupData.RspData, Remarkdata.RspData);
+				} else {
+					list = addRemarkData(groupData.RspData)
+				}
+				events.clearChild(item);
+				console.log('最终呈现的数据：' + vvl + JSON.stringify(list));
+				createGride(item, list);
+			})
+
+		}
+	});
+}
+var addRemarkData = function(list, remarkList) {
+		if(remarkList) {
+			for(var i in list) {
+				for(var j in remarkList) {
+					if(list[i].utid == remarkList[j].butid) {
+						list[i].bunick = remarkList[j].bunick;
+						break;
+					} else {
+						list[i].bunick = list[i].gname;
+					}
 				}
 			}
+			//		list.forEach(function(cell,j,tolist){
+			//			remarkList.forEach(function(remark,i,relist){
+			//				console.log('对比值：'+JSON.stringify(cell)+':'+JSON.stringify(remark));
+			//				if(cell.utid==remark.butid){
+			//					list[j].bunick=remark.bunick;
+			//					return false;
+			//				}else{
+			//					list[j].bunick=cell.ugname;
+			//				}
+			//			})
+			//		})
+		} else {
+			list.forEach(function(cell, i) {
+				list[i].bunick = cell.ugname;
+			})
 		}
-//		list.forEach(function(cell,j,tolist){
-//			remarkList.forEach(function(remark,i,relist){
-//				console.log('对比值：'+JSON.stringify(cell)+':'+JSON.stringify(remark));
-//				if(cell.utid==remark.butid){
-//					list[j].bunick=remark.bunick;
-//					return false;
-//				}else{
-//					list[j].bunick=cell.ugname;
-//				}
-//			})
-//		})
-	}else{
-		list.forEach(function(cell,i){
-			list[i].bunick=cell.ugname;
-		})
+		return list;
 	}
-	return list;
-}
-/**
- * 获取备注
- */
-var getRemarkData = function(list,callback) {
+	/**
+	 * 获取备注
+	 */
+var getRemarkData = function(list, callback) {
 		var wd = plus.nativeUI.showWaiting(storageKeyName.WAITING);
-		var utids=[];
-		list.forEach(function(cell){
+		var utids = [];
+		list.forEach(function(cell) {
 			utids.push(cell.utid);
 		})
-		console.log('传的字符串：'+utids.toString())
+		console.log('传的字符串：' + utids.toString())
 		postDataPro_PostUmk({
 			vvl: utids.toString()
 		}, wd, function(data) {
 			wd.close();
-			console.log('获取的备注信息：'+JSON.stringify(data));
-			var remark=document.getElementById('person-remark');
+			console.log('获取的备注信息：' + JSON.stringify(data));
+			var remark = document.getElementById('person-remark');
 			callback(data);
 		})
 	}
@@ -333,33 +339,33 @@ var getRemarkData = function(list,callback) {
 	 */
 var createGride = function(gride, array) {
 
-	//数组遍历
-	array.forEach(
-		/**
-		 * 创建子元素
-		 * @param {Object} map 数组元素
-		 * @param {Object} index 数组序号
-		 * @param {Object} array 数组
-		 */
-		function(cell, index, array) {
-			var li = document.createElement('li'); //子元素
-			//			var bgColor=getRandomColor();//获取背景色
-			if(array.length <= 3) { //数组小于等于3，每行3个图标
-				li.className = "mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4";
-			} else { //数组大于3，每行四个图标
-				li.className = "mui-table-view-cell mui-media mui-col-xs-3 mui-col-sm-3";
-			}
-			cell.gname=groupName;
-			li.info = cell;
-			//子控件的innerHTML
-			li.innerHTML = '<a href="#">' +
-				'<img class="circular-square" src="' + getImg(cell.uimg) + '"/></br>' +
-				'<small class="">' + cell.bunick + '</small>' +
-				'</a>';
-			gride.appendChild(li);
-		})
-}
-//頭像設置
+		//数组遍历
+		array.forEach(
+			/**
+			 * 创建子元素
+			 * @param {Object} map 数组元素
+			 * @param {Object} index 数组序号
+			 * @param {Object} array 数组
+			 */
+			function(cell, index, array) {
+				var li = document.createElement('li'); //子元素
+				//			var bgColor=getRandomColor();//获取背景色
+				if(array.length <= 3) { //数组小于等于3，每行3个图标
+					li.className = "mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4";
+				} else { //数组大于3，每行四个图标
+					li.className = "mui-table-view-cell mui-media mui-col-xs-3 mui-col-sm-3";
+				}
+				cell.gname = groupName;
+				li.info = cell;
+				//子控件的innerHTML
+				li.innerHTML = '<a href="#">' +
+					'<img class="circular-square" src="' + getImg(cell.uimg) + '"/></br>' +
+					'<small class="">' + cell.bunick + '</small>' +
+					'</a>';
+				gride.appendChild(li);
+			})
+	}
+	//頭像設置
 var getImg = function(img) {
 	return img == null ? "../../image/utils/default_personalimage.png" : img
 }
