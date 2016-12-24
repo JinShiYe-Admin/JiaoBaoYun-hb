eb3d9576648e7fb5e67b7ff6e54919d195cc744a
rockan007 <rockan007@gmail.com>
2016-12-24 09:38:44 +0800

    提交上传图片代码

--- JiaoBaoYun-hb/js/utils/files.js
+++ JiaoBaoYun-hb/js/utils/files.js
@@ -1,73 +1,73 @@
 var files = (function(mod) {
-		mod.getFilesByWWW = function() {
-			mod.getFiles(plus.io.PRIVATE_WWW);
-		};
-		mod.getFilesByDOC = function() {
-			mod.getFiles(plus.io.PRIVATE_DOC);
-		};
-		mod.getFilesPDOC = function() {
-			mod.getFiles(plus.io.PUBLIC_DOCUMENTS);
-		};
-		mod.getFilesDownload = function() {
-			mod.getFiles(plus.io.PUBLIC_DOWNLOADS);
-		};
-		mod.getFiles = function(type) {
-			plus.io.requestFileSystem(type, function(fs) {
-				// 可通过fs进行文件操作 
-				mod.readDirectory(fs.root)
-				console.log("files" + fs.name);
-			}, function(e) {
-				alert("Request file system failed: " + e.message);
-			});
-		}
-		mod.readDirectory = function(directory) {
-			var directoryReader = directory.createReader();
-			var entries = [];
-			console.log("whatever")
-			directoryReader.readEntries(function(entries) {
-				var i;
-				for(i = 0; i < entries.length; i++) {
-					console.log(JSON.stringify(entries[i].name));
-				}
-			}, function(e) {
-				alert("Read entries failed: " + e.message);
-			});
+	mod.getFilesByWWW = function() {
+		mod.getFiles(plus.io.PRIVATE_WWW);
+	};
+	mod.getFilesByDOC = function() {
+		mod.getFiles(plus.io.PRIVATE_DOC);
+	};
+	mod.getFilesPDOC = function() {
+		mod.getFiles(plus.io.PUBLIC_DOCUMENTS);
+	};
+	mod.getFilesDownload = function() {
+		mod.getFiles(plus.io.PUBLIC_DOWNLOADS);
+	};
+	mod.getFiles = function(type) {
+		plus.io.requestFileSystem(type, function(fs) {
+			// 可通过fs进行文件操作 
+			mod.readDirectory(fs.root)
+			console.log("files" + fs.name);
+		}, function(e) {
+			alert("Request file system failed: " + e.message);
+		});
+	}
+	mod.readDirectory = function(directory) {
+		var directoryReader = directory.createReader();
+		var entries = [];
+		console.log("whatever")
+		directoryReader.readEntries(function(entries) {
+			var i;
+			for(i = 0; i < entries.length; i++) {
+				console.log(JSON.stringify(entries[i].name));
+			}
+		}, function(e) {
+			alert("Read entries failed: " + e.message);
+		});
 
+	}
+	mod.getFileByPath = function(path, callback) {
+			plus.io.resolveLocalFileSystemURL(path, function(entry) {
+				//			console.log(JSON.stringify(entry.File.fileName))
+				// Read data from file
+				var abPath = entry.toLocalURL() + "?version=" + new Date().getTime();
+				var img = new Image();
+				img.src = abPath;
+				var imgData = getBase64Image(img);
+				console.log(imgData);
+				callback(imgData);
+				//			var reader = null;
+				//			entry.file(function(file) {
+				//				reader = new plus.io.FileReader();
+				//				console.log("getFile:" + JSON.stringify(file));
+				//				reader.onloadend = function(evt) {
+				//					console.log("Read success:"+evt.target.result);
+				//					// Get data
+				//					var fileStream=evt.target.result
+				//					
+				//					console.log(fileStream);
+				//					
+				//					callback(fileStream);
+				//				};
+				//				reader.readAsBinaryString(file)
+				//				reader.readAsDataURL(file);
+			}, function(e) {
+				console('获取文件流错误：' + e.message);
+			});
 		}
-		mod.getFileByPath = function(path, callback) {
-				plus.io.resolveLocalFileSystemURL(path, function(entry) {
-					//			console.log(JSON.stringify(entry.File.fileName))
-					// Read data from file
-					var abPath  = entry.toLocalURL() + "?version=" + new Date().getTime(); 
-					var img = new Image();
-					img.src = abPath;
-					 var imgData = getBase64Image(img); 
-					 console.log(imgData);
-					callback(imgData);
-					//			var reader = null;
-					//			entry.file(function(file) {
-					//				reader = new plus.io.FileReader();
-					//				console.log("getFile:" + JSON.stringify(file));
-					//				reader.onloadend = function(evt) {
-					//					console.log("Read success:"+evt.target.result);
-					//					// Get data
-					//					var fileStream=evt.target.result
-					//					
-					//					console.log(fileStream);
-					//					
-					//					callback(fileStream);
-					//				};
-					//				reader.readAsBinaryString(file)
-					//				reader.readAsDataURL(file);
-				}, function(e) {
-					console('获取文件流错误：' + e.message);
-				});
-		}
-//			},
-//			function(e) {
-//				console.log(e.message);
-//			}
-	//将图片压缩转成base64 
+		//			},
+		//			function(e) {
+		//				console.log(e.message);
+		//			}
+		//将图片压缩转成base64 
 	function getBase64Image(img) {
 		var canvas = document.createElement("canvas");
 		var width = img.width;
