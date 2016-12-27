af9b787673f655be84f99f9f6a6d1e7e4b8b8cd1
rockan007 <rockan007@gmail.com>
2016-12-27 09:55:44 +0800

    回复框

--- JiaoBaoYun-hb/js/quan/about-me.js
+++ JiaoBaoYun-hb/js/quan/about-me.js
@@ -23,6 +23,7 @@ var repliedCell;
 var aboutMeArray = [];
 mui.init();
 mui.plusReady(function() {
+		initNativeObjects();
 		//页码1
 		pageIndex = 1;
 		//请求并放置数据
@@ -96,9 +97,7 @@ var addReplyView = function() {
 	mui('.mui-table-view').on('tap', '.reply', function() {
 		var replyContainer = document.getElementById('footer');
 		replyContainer.style.display = 'block';
-		setTimeout(function(){
-			jQuery('textarea').focus();	
-		},200)
+		showSoftInput('#msg-content');
 		repliedCell = this.cell;
 		console.log('点击的回复包含数据：' + JSON.stringify(repliedCell));
 		msgType = this.cell.MsgType;
