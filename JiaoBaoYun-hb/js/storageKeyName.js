32d82ee7e2d78c1ab668caf9b19b67596f4962da
BugHunter1234 <444811716@qq.com>
2016-12-29 12:47:35 +0800

    调整笔记上拉加载，调整云盘首页，修改昵称后修改云盘首页，增加默认个人头像到七牛

--- JiaoBaoYun-hb/js/storageKeyName.js
+++ JiaoBaoYun-hb/js/storageKeyName.js
@@ -6,10 +6,10 @@ var storageKeyName = (function(mod) {
 	mod.AUTOLOGIN = 'autoLogin'; //登录信息
 
 	//---开发---start---
-//	mod.MAINURL = 'http://192.168.0.44:8511/api/CloudApi/'; //主url
-//	mod.MAINJIAOXIAOURL = 'http://192.168.0.106:8081/JiaoBaoCloudService/'; //家校圈url
-//	mod.MAINHOMEWORKURL = 'http://192.168.0.211:802/' //作业主url
-	//---开发---end---
+	mod.MAINURL = 'http://192.168.0.44:8511/api/CloudApi/'; //主url
+	mod.MAINJIAOXIAOURL = 'http://192.168.0.106:8081/JiaoBaoCloudService/'; //家校圈url
+	mod.MAINHOMEWORKURL = 'http://192.168.0.211:802/' //作业主url
+		//---开发---end---
 
 	//---测试---start---
 	//mod.MAINURL = 'http://192.168.0.178:8511/api/CloudApi/';//主url
@@ -18,9 +18,9 @@ var storageKeyName = (function(mod) {
 	//---测试---end---
 
 	//---外网---start---
-	mod.MAINURL = 'http://114.215.222.186:8002/api/CloudApi/'; //主url
-	mod.MAINJIAOXIAOURL = 'http://114.215.222.194:8080/JiaoBaoCloudService/'; //家校圈url
-	mod.MAINHOMEWORKURL = 'http://114.215.222.186:8001/' //作业主url
+	//mod.MAINURL = 'http://114.215.222.186:8002/api/CloudApi/'; //主url
+	//mod.MAINJIAOXIAOURL = 'http://114.215.222.194:8080/JiaoBaoCloudService/'; //家校圈url
+	//mod.MAINHOMEWORKURL = 'http://114.215.222.186:8001/' //作业主url
 	//---外网---end---
 
 	mod.MAINHOMEWORKURLTEACHER = mod.MAINHOMEWORKURL + 'TeacherService.svc/'; //老师作业url
@@ -31,7 +31,7 @@ var storageKeyName = (function(mod) {
 	mod.QNFILEDOMAIN = 'http://oigrn7z7d.bkt.clouddn.com/'; //存放在七牛的文件（云存储的文件）的域名，私有的,jiaobaoyun-file
 	mod.QNFILEIMGEDOMAIN = 'http://oigrmug2o.bkt.clouddn.com/'; //存放在七牛的文件（云存储的图片，视频文件）的缩略图域名，公开的,jiaobaoyun-filethumbnail
 	mod.QNHEADIMGEDOMAIN = 'http://oigr7qsvz.bkt.clouddn.com/'; //存放在七牛的个人头像，群头像，资料头像的域名，公开的，jiaobaoyun-headimage
-
+	mod.QNDEFAULTIMAGEDOMAIN = 'http://oixh9lre3.bkt.clouddn.com/'; //存放在七牛的默认图片的域名，公开的
 	//---测试---start---
 	mod.QNGETUPTOKENHEADIMGE = 'http://192.168.0.178:8517/GetTokenProfilePhoto.ashx'; //获取上传个人头像，群头像，资料头像到七牛的token的url
 	mod.QNGETUPTOKENFILE = 'http://192.168.0.178:8517/QiuToken.ashx'; //获取上传文件（云存储）到七牛的token的url
@@ -45,8 +45,10 @@ var storageKeyName = (function(mod) {
 	//mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8003/GetToken.ashx?geturl='; //获取下载文件（云存储）的token的url，url+七牛文件url
 	//mod.QNGETTOKENDELETE = 'http://114.215.222.186:8003/BatchDelete.ashx'; //获取批量（或者一个）删除七牛文件的token的url
 	//---外网---end---
-	//---七牛---start---
+	//---七牛---end---
 	mod.DOCUMENTSPATH = 'DOCUMENTSPATH'; //记录document的地址
+	mod.DEFAULTPERSONALHEADIMAGE = mod.QNDEFAULTIMAGEDOMAIN + 'default_personalimage.png'; //默认的个人头像
+
 	return mod;
 
 })(storageKeyName || {});
