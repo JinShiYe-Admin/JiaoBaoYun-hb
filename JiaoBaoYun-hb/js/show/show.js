23adc397b639a75e6c7eec166fa6eccbd09e3117
Zqw <578159259@qq.com>
2017-01-14 11:38:10 +0800

    定制城市协议数据

--- JiaoBaoYun-hb/js/show/show.js
+++ JiaoBaoYun-hb/js/show/show.js
@@ -5,7 +5,7 @@ mui.plusReady(function() {
 	window.addEventListener('tapTitleLeft', function() {
 		console.log('展现-tapTitleLeft');
 		events.openNewWindowWithData('../utils/customizeCity.html', {
-			id: 'show',
+			id: '1',
 			webid: '../show/show_home.html', //当前webview的id
 			cities: cities //已经定制的城市数组
 		});
