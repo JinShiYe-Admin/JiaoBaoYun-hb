831d673e1f830b645c16ec45f80145ad9270ee0f
rockan007 <rockan007@gmail.com>
2017-01-17 14:34:16 +0800

    求知详情

--- JiaoBaoYun-hb/js/qiuzhi/qiuzhi-questionSub.js
+++ JiaoBaoYun-hb/js/qiuzhi/qiuzhi-questionSub.js
@@ -3,32 +3,33 @@
  */
 mui.init();
 mui.plusReady(function() {
-	window.addEventListener('askId',function(e){
-		console.log('问题详情页面获取的问题id:'+e.detail.data);
+		window.addEventListener('askId', function(e) {
+				console.log('问题详情页面获取的问题id:' + e.detail.data);
+			})
+			//加载刷新
+		events.initRefresh("控件id",
+			function() { //刷新方法
+
+			},
+			function() { //加载更多
+
+			});
 	})
-	//加载刷新
-	events.initRefresh("控件id",
-	function() {//刷新方法
-		
-	}, function() {//加载更多
-		
-	});
-})
-/**
- * 请求问题
- */
-/**
- * 请求答案列表
- */
-/**
- * 放置问题
- */
-/**
- * 放置答案列表
- */
-/**
- * 各种监听事件
- */
-var setListener=function(){
-	
-}
+	/**
+	 * 请求问题
+	 */
+	/**
+	 * 请求答案列表
+	 */
+	/**
+	 * 放置问题
+	 */
+	/**
+	 * 放置答案列表
+	 */
+	/**
+	 * 各种监听事件
+	 */
+var setListener = function() {
+
+	}
