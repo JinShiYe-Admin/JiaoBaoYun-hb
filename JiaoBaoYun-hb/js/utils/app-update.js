21efa4d921fd28ec9854248d48e42a7b1d354eaa
rockan007 <rockan007@gmail.com>
2017-02-09 15:10:36 +0800

    修改检测版本的错误，下载提示框

--- JiaoBaoYun-hb/js/utils/app-update.js
+++ JiaoBaoYun-hb/js/utils/app-update.js
@@ -1,36 +1,74 @@
+/**
+ * 更新版本模块
+ */
 var appUpdate = (function(mod) {
+	/**
+	 * 获取版本信息后，判断是否更新
+	 * @param {Object} versionInfo 服务器返回的版本信息
+	 */
 	mod.getAppVersion = function(versionInfo) {
-		plus.runtime.getProperty(plus.runtime.appid, function(inf) {
-			mod.appVersion = inf.version;
-			console.log("当前应用版本：" + mod.appVersion);
-			getUpCondition(versionInfo);
-		});
-	}
+			plus.runtime.getProperty(plus.runtime.appid, function(inf) {
+				mod.appVersion = inf.version;
+				console.log("当前应用版本：" + mod.appVersion);
+				getUpCondition(versionInfo); //判断是否更新
+			});
+		}
+		/**
+		 * 判断是否更新
+		 * @param {Object} versionInfo
+		 */
 	var getUpCondition = function(versionInfo) {
 		var appVersions = mod.appVersion.split('.');
 		var newestVersions = versionInfo.ver.split('.');
 		var appVersionMinMax = getMinMax(appVersions);
 		var newestVersionMinMax = getMinMax(newestVersions);
-		if(appVersionMinMax.max < newestVersionMinMax.max) {
-			downApk(versionInfo.baseverurl);
+		if(appVersionMinMax.max < newestVersionMinMax.max) { //整包更新
+			//询问是否更新
+			setDialog('教宝云有新版本，是否下载？', function() {
+				downApk(versionInfo.baseverurl)
+			})
 		} else {
-			if(appVersionMinMax.min<newestVersionMinMax.min){
-				downWgt(versionInfo.addverurl);
+			if(appVersionMinMax.min < newestVersionMinMax.min) { //在线更新
+				setDialog('教宝云有新版本，是否下载？', function() {
+					downWgt(versionInfo.addverurl);
+				})
 			}
 		}
 	}
+	/**
+	 * 设置提示对话框
+	 * @param {Object} hint 提示语
+	 * @param {Object} callback 确认后的回调函数
+	 */
+	var setDialog = function(hint, callback) {
+			var btnArray = ['否', '是'];
+			mui.confirm(hint, '教宝云', btnArray, function(e) {
+				if(e.index == 1) {
+					callback();
+				} else {
+					mui.toast('您已取消下载新版本！')
+				}
+			})
+		}
+		/**
+		 * 获取大版本号和小版本号
+		 * @param {Object} numArray
+		 */
 	var getMinMax = function(numArray) {
-		var minMax = {};
-		for(var i in numArray) {
-			if(i ==0) {
-				minMax.max = parseInt(numArray[i]);
-			} else {
-				minMax.min += parseInt(numArray[i]);
+			var minMax = {};
+			for(var i in numArray) {
+				if(i == 0) {
+					minMax.max = parseInt(numArray[i]);
+				} else {
+					minMax.min += parseInt(numArray[i]);
+				}
 			}
+			return minMax;
 		}
-		return minMax;
-	}
-
+		/**
+		 * 下载整包
+		 * @param {Object} ApkUrl 整包地址
+		 */
 	function downApk(ApkUrl) {
 		if(plus.os.android) {
 			var url = ""; // 下载文件地址
@@ -40,13 +78,16 @@ var appUpdate = (function(mod) {
 					console.log(d.filename);
 					installApk(path);
 				} else { //下载失败
-					alert("Download failed: " + status);
+					mui.toast("Download failed: " + status);
 				}
 			});
 			dtask.start();
 		}
 	}
-
+	/**
+	 * 下载在线更新的资源
+	 * @param {Object} wgtUrl
+	 */
 	function downWgt(wgtUrl) {
 		plus.nativeUI.showWaiting("下载wgt文件...");
 		plus.downloader.createDownload(wgtUrl, {
@@ -62,7 +103,10 @@ var appUpdate = (function(mod) {
 			plus.nativeUI.closeWaiting();
 		}).start();
 	}
-
+	/**
+	 * 装载正整包
+	 * @param {Object} path
+	 */
 	function installApk(path) {
 		if(plus.os.android) {
 			plus.runtime.install(path); // 安装下载的apk文件
@@ -71,7 +115,10 @@ var appUpdate = (function(mod) {
 			plus.runtime.openURL(url);
 		}
 	}
-	// 更新应用资源
+	/**
+	 * 加载在线安装包
+	 * @param {Object} path
+	 */
 	function installWgt(path) {
 		plus.nativeUI.showWaiting("安装wgt文件...");
 		plus.runtime.install(path, {}, function() {
@@ -86,4 +133,5 @@ var appUpdate = (function(mod) {
 			plus.nativeUI.alert("安装wgt文件失败[" + e.code + "]：" + e.message);
 		});
 	}
+	return mod;
 })(appUpdate || {})
