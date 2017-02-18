8ac192bf917c369c9df8fd65cd9ce9c3920f812d
BugHunter1234 <444811716@qq.com>
2017-02-18 09:40:16 +0800

    增加退出登录后执行的方法，重置云盘主页

--- JiaoBaoYun-hb/js/utils/events.js
+++ JiaoBaoYun-hb/js/utils/events.js
@@ -9,55 +9,55 @@ var events = (function(mod) {
 	 * @param {Object} event 绑定的监听事件
 	 */
 	mod.addTap = function(id, event) {
-			var item = document.getElementById(id);
-			item.addEventListener('tap', event);
-		}
-		/**
-		 * 加载跳转界面监听的公用方法
-		 * @param {Object} item 加载监听的控件
-		 * @param {Object} targetHTML 目标Url
-		 */
+		var item = document.getElementById(id);
+		item.addEventListener('tap', event);
+	}
+	/**
+	 * 加载跳转界面监听的公用方法
+	 * @param {Object} item 加载监听的控件
+	 * @param {Object} targetHTML 目标Url
+	 */
 	mod.jumpPage = function(item, targetHTML) {
-			item.addEventListener('tap', function() {
-				mod.openNewWindow(targetHTML);
+		item.addEventListener('tap', function() {
+			mod.openNewWindow(targetHTML);
+		})
+	}
+	/**
+	 * 打开新界面
+	 * @param {Object} targetPage 目标界面
+	 */
+	mod.openNewWindow = function(tarPagePath) {
+		var tarPageIds = tarPagePath.split('/');
+		var targetPage = plus.webview.getWebviewById(tarPageIds[tarPageIds.length - 1]);
+		console.log('targetPage是否存在:' + Boolean(targetPage))
+		if(targetPage) {
+			targetPage.show();
+		} else {
+			mui.openWindow({
+				url: tarPagePath,
+				id: tarPageIds[tarPageIds.length - 1],
+				show: {
+					anishow: 'slide-in-right',
+					duration: 250
+				},
+				waiting: {
+					title: '正在加载...'
+				},
+				styles: {
+					top: '0px',
+					bottom: '0px'
+				}
 			})
 		}
-		/**
-		 * 打开新界面
-		 * @param {Object} targetPage 目标界面
-		 */
-	mod.openNewWindow = function(tarPagePath) {
-			var tarPageIds = tarPagePath.split('/');
-			var targetPage = plus.webview.getWebviewById(tarPageIds[tarPageIds.length - 1]);
-			console.log('targetPage是否存在:' + Boolean(targetPage))
-			if(targetPage) {
-				targetPage.show();
-			} else {
-				mui.openWindow({
-					url: tarPagePath,
-					id: tarPageIds[tarPageIds.length - 1],
-					show: {
-						anishow: 'slide-in-right',
-						duration: 250
-					},
-					waiting: {
-						title: '正在加载...'
-					},
-					styles: {
-						top: '0px',
-						bottom: '0px'
-					}
-				})
-			}
 
-		}
-		/**
-		 * 打开新页面时，同时传值
-		 * 扩展参数仅在打开新窗口时有效，若目标窗口为预加载页面，
-		 * 则通过mui.openWindow方法打开时传递的extras参数无效。
-		 * @param {Object} targetHTML 新页面路径
-		 * @param {Object} passData 获取要传的值
-		 */
+	}
+	/**
+	 * 打开新页面时，同时传值
+	 * 扩展参数仅在打开新窗口时有效，若目标窗口为预加载页面，
+	 * 则通过mui.openWindow方法打开时传递的extras参数无效。
+	 * @param {Object} targetHTML 新页面路径
+	 * @param {Object} passData 获取要传的值
+	 */
 	mod.openNewWindowWithData = function(targetHTML, passData) {
 		mui.openWindow({
 			url: targetHTML,
@@ -115,175 +115,175 @@ var events = (function(mod) {
 	 */
 	mod.initRefresh = function(id, fresh, addMore) {
 
-			mui.init({
-				pullRefresh: {
-					container: '#refreshContainer',
-					down: {
-						callback: pulldownRefresh
-					},
-					up: {
-						contentrefresh: '正在加载...',
-						callback: pullupRefresh
-					}
+		mui.init({
+			pullRefresh: {
+				container: '#refreshContainer',
+				down: {
+					callback: pulldownRefresh
+				},
+				up: {
+					contentrefresh: '正在加载...',
+					callback: pullupRefresh
 				}
-			});
-			/**
-			 * 下拉刷新具体业务实现
-			 */
-			function pulldownRefresh() {
-				setTimeout(function() {
-					mui('#refreshContainer').pullRefresh().refresh(true);
-					var item = document.getElementById(id)
-						//清除所有数据
-					mod.clearChild(item);
-					//					while(item.firstChild != null) {
-					//						item.removeChild(item.firstChild)
-					//					}
-					//加载新控件
-					fresh();
-					mui('#refreshContainer').pullRefresh().endPulldownToRefresh(); //refresh completed
-				}, 150);
 			}
-			var count = 0;
-			/**
-			 * 上拉加载具体业务实现
-			 */
-			function pullupRefresh() {
-				setTimeout(function() {
-					addMore();
-				}, 1500);
-			}
-		}
+		});
 		/**
-		 * 预加载单个页面 在mui.plusReady里调用
-		 * @param {Object} tarPage 页面路径
-		 * @param {Object} interval 延迟加载时间间隔 单位毫秒 ，不输入默认为0
+		 * 下拉刷新具体业务实现
 		 */
+		function pulldownRefresh() {
+			setTimeout(function() {
+				mui('#refreshContainer').pullRefresh().refresh(true);
+				var item = document.getElementById(id)
+				//清除所有数据
+				mod.clearChild(item);
+				//					while(item.firstChild != null) {
+				//						item.removeChild(item.firstChild)
+				//					}
+				//加载新控件
+				fresh();
+				mui('#refreshContainer').pullRefresh().endPulldownToRefresh(); //refresh completed
+			}, 150);
+		}
+		var count = 0;
+		/**
+		 * 上拉加载具体业务实现
+		 */
+		function pullupRefresh() {
+			setTimeout(function() {
+				addMore();
+			}, 1500);
+		}
+	}
+	/**
+	 * 预加载单个页面 在mui.plusReady里调用
+	 * @param {Object} tarPage 页面路径
+	 * @param {Object} interval 延迟加载时间间隔 单位毫秒 ，不输入默认为0
+	 */
 	mod.preload = function(tarPage, interval) {
-			if(!interval) {
-				interval = 0;
-			}
-			if(!plus.webview.getWebviewById(tarPage)) {
-				//初始化预加载详情页面
-				setTimeout(function() {
-					mui.preload({
-						url: tarPage,
-						id: tarPage.split('/')[tarPage.split('/').length - 1], //默认使用当前页面的url作为id
-						styles: { //窗口参数
-							top: '0px',
-							bottom: '0px'
-						},
-						show: {
-							anishow: 'slide-in-right',
-							duration: 250
-						},
-						waiting: {
-							title: '正在加载...'
-						}
-					})
-				}, interval)
-			}
-
+		if(!interval) {
+			interval = 0;
 		}
-		/**
-		 * 加载不需要传值的预加载页面
-		 * @param {Object} tarpge
-		 */
+		if(!plus.webview.getWebviewById(tarPage)) {
+			//初始化预加载详情页面
+			setTimeout(function() {
+				mui.preload({
+					url: tarPage,
+					id: tarPage.split('/')[tarPage.split('/').length - 1], //默认使用当前页面的url作为id
+					styles: { //窗口参数
+						top: '0px',
+						bottom: '0px'
+					},
+					show: {
+						anishow: 'slide-in-right',
+						duration: 250
+					},
+					waiting: {
+						title: '正在加载...'
+					}
+				})
+			}, interval)
+		}
+
+	}
+	/**
+	 * 加载不需要传值的预加载页面
+	 * @param {Object} tarpge
+	 */
 	mod.showPreloadPage = function(tarPage) {
-			var targetPage = null;
-			//获得目标页面
-			if(!targetPage) {
-				targetPage = plus.webview.getWebviewById(tarPage);
+		var targetPage = null;
+		//获得目标页面
+		if(!targetPage) {
+			targetPage = plus.webview.getWebviewById(tarPage);
 
-			}
-			targetPage.show('slide-in-right', 250);
 		}
-		/**
-		 * 如果目标页面未加载,需要先预加载页面
-		 * 传递数值到指定页面并打开页面
-		 * @param {Object} tarpage 目标页面Id
-		 * @param {Object} listener 监听事件
-		 * @param {Object} getDatas 获取数据的方法  return somthing
-		 */
+		targetPage.show('slide-in-right', 250);
+	}
+	/**
+	 * 如果目标页面未加载,需要先预加载页面
+	 * 传递数值到指定页面并打开页面
+	 * @param {Object} tarpage 目标页面Id
+	 * @param {Object} listener 监听事件
+	 * @param {Object} getDatas 获取数据的方法  return somthing
+	 */
 	mod.fireToPage = function(tarPage, listener, getDatas) {
-			//			console.log('tarPage:' + tarPage);
-			tarPage = tarPage.split('/')[tarPage.split('/').length - 1];
-			var targetPage = null;
-			//获得目标页面
-			if(!targetPage) {
-				targetPage = plus.webview.getWebviewById(tarPage);
-				//				console.log(typeof(targetPage))
-			}
-			//触发目标页面的listener事件
-			mui.fire(targetPage, listener, {
-				data: getDatas()
-			});
-			console.log('要传的值是：' + JSON.stringify(getDatas()))
-			targetPage.show('slide-in-right', 250)
+		//			console.log('tarPage:' + tarPage);
+		tarPage = tarPage.split('/')[tarPage.split('/').length - 1];
+		var targetPage = null;
+		//获得目标页面
+		if(!targetPage) {
+			targetPage = plus.webview.getWebviewById(tarPage);
+			//				console.log(typeof(targetPage))
 		}
-		/**
-		 * 如果目标页面未加载,需要先预加载页面
-		 * 传递数值到指定页面并打开页面
-		 * @param {Object} tarpage 目标页面Id
-		 * @param {Object} listener 监听事件
-		 * @param {Object} datas 要传递的数据
-		 */
+		//触发目标页面的listener事件
+		mui.fire(targetPage, listener, {
+			data: getDatas()
+		});
+		console.log('要传的值是：' + JSON.stringify(getDatas()))
+		targetPage.show('slide-in-right', 250)
+	}
+	/**
+	 * 如果目标页面未加载,需要先预加载页面
+	 * 传递数值到指定页面并打开页面
+	 * @param {Object} tarpage 目标页面Id
+	 * @param {Object} listener 监听事件
+	 * @param {Object} datas 要传递的数据
+	 */
 	mod.fireToPageWithData = function(tarPage, listener, datas) {
-			console.log('tarPage:' + tarPage);
-			tarPage = tarPage.split('/')[tarPage.split('/').length - 1];
-			var targetPage = null;
-			//获得目标页面
-			if(!targetPage) {
-				targetPage = plus.webview.getWebviewById(tarPage);
-				//				console.log(typeof(targetPage));
-			}
+		console.log('tarPage:' + tarPage);
+		tarPage = tarPage.split('/')[tarPage.split('/').length - 1];
+		var targetPage = null;
+		//获得目标页面
+		if(!targetPage) {
+			targetPage = plus.webview.getWebviewById(tarPage);
+			//				console.log(typeof(targetPage));
+		}
+		//触发目标页面的listener事件
+		mui.fire(targetPage, listener, {
+			data: datas
+		});
+		targetPage.show('slide-in-right', 250)
+	}
+	/**
+	 * 事件传递 不传数据 常用于 父子页面间
+	 * @param {Object} tarPage 目标页面
+	 * @param {Object} listener 事件
+	 */
+	mod.fireToPageNone = function(tarPage, listener, datas) {
+		tarPage = tarPage.split('/')[tarPage.split('/').length - 1];
+		if(!datas) {
+			datas = null;
+		}
+		console.log('tarPage:' + tarPage);
+		var targetPage = null;
+		//获得目标页面
+		if(!targetPage) {
+			targetPage = plus.webview.getWebviewById(tarPage);
+		}
+		if(targetPage) {
 			//触发目标页面的listener事件
 			mui.fire(targetPage, listener, {
 				data: datas
 			});
-			targetPage.show('slide-in-right', 250)
+		} else {
+			console.log('目标页面不存在' + tarPage);
 		}
-		/**
-		 * 事件传递 不传数据 常用于 父子页面间
-		 * @param {Object} tarPage 目标页面
-		 * @param {Object} listener 事件
-		 */
-	mod.fireToPageNone = function(tarPage, listener, datas) {
-			tarPage = tarPage.split('/')[tarPage.split('/').length - 1];
-			if(!datas) {
-				datas = null;
-			}
-			console.log('tarPage:' + tarPage);
-			var targetPage = null;
-			//获得目标页面
-			if(!targetPage) {
-				targetPage = plus.webview.getWebviewById(tarPage);
-			}
-			if(targetPage) {
-				//触发目标页面的listener事件
-				mui.fire(targetPage, listener, {
-					data: datas
-				});
-			} else {
-				console.log('目标页面不存在' + tarPage);
-			}
 
-		}
-		/**
-		 * 清空子元素
-		 * @param {Object} item 需清空子元素的控件
-		 */
+	}
+	/**
+	 * 清空子元素
+	 * @param {Object} item 需清空子元素的控件
+	 */
 	mod.clearChild = function(item) {
-			while(item.firstElementChild) {
-				item.removeChild(item.firstElementChild);
-			}
+		while(item.firstElementChild) {
+			item.removeChild(item.firstElementChild);
 		}
-		/**
-		 * listView加载数据
-		 * @param {Object} listContainer
-		 * @param {Object} data
-		 * @param {Object} createInner
-		 */
+	}
+	/**
+	 * listView加载数据
+	 * @param {Object} listContainer
+	 * @param {Object} data
+	 * @param {Object} createInner
+	 */
 	mod.createListView = function(listContainer, data, createInner) {
 		var list = document.getElementById(listContainer);
 		data.forEach(function(cell, i, data) {
@@ -390,7 +390,7 @@ var events = (function(mod) {
 	mod.changeTopPocket = function(top) {
 		setTimeout(function() {
 			var toppocket = document.querySelector('.mui-pull-top-pocket');
-			var height;//高度
+			var height; //高度
 			if(top) {
 				height = top;
 			} else {
@@ -399,14 +399,26 @@ var events = (function(mod) {
 			if(toppocket) {
 				document.querySelector('.mui-pull-top-pocket').style.top = height + 'px';
 			} else {
-				mod.changeTopPocket();
+				mod.changeTopPocket(height);
 			}
 		}, 200);
 	}
-	mod.getFileNameByPath=function(filePath){
-		var filePaths=filePath.split("/");
-		return filePaths[filePaths.length-1];
+	mod.getFileNameByPath = function(filePath) {
+		var filePaths = filePath.split("/");
+		return filePaths[filePaths.length - 1];
 	}
+
+	/**
+	 * 退出登录后执行的方法
+	 * @author 莫尚霖
+	 */
+	mod.logOff = function() {
+		//清理上传下载的任务和界面
+		events.fireToPageNone('storage_transport.html', 'removeAllTask');
+		//清理云盘主页
+		events.fireToPageNone('../cloud/cloud_home.html', 'cleanCloudHome');
+	}
+
 	return mod;
 
 })(events || {});
