15dcce559698385572c363030035accc2702a5a5
rockan007 <rockan007@gmail.com>
2017-02-28 10:11:39 +0800

    发布作业加载相册功能

--- JiaoBaoYun-hb/js/homework/homework-publish.js
+++ JiaoBaoYun-hb/js/homework/homework-publish.js
@@ -10,95 +10,141 @@ var subjectsContainer = document.getElementById('subjects');
 var personalUTID;
 mui.init();
 mui.plusReady(function() {
-		events.preload('classes-select.html', 200);
-		window.addEventListener('postClasses', function(e) {
-			CloudFileUtil.files=[];
-			personalUTID = parseInt(window.myStorage.getItem(window.storageKeyName.PERSONALINFO).utid);
-			console.log('发布作业界面获取的班级数据：' + JSON.stringify(e.detail.data));
-			var switchItem=document.getElementById("onlineSwitch");
-			var isActive = switchItem.classList.contains("mui-active");
-			if(!isActive){
-				mui('#onlineSwitch').switch().toggle();
-			}
-			document.getElementById('publish-content').value=null;
-			//选中班级为全部班级
-			selectClassArray = e.detail.data;
-			//请求所有班级学生数据
-			requestClassStudents();
-			//科目
-			requestSubjectList(setSubjects);
-		})
-		events.addTap('select-classes', function() {
-				events.fireToPageWithData('classes-select.html', 'postClasses', selectClassArray);
-			})
-			/**
-			 * 监听选择班级后的返回数据
-			 */
-		window.addEventListener('selectedClasses', function(e) {
-				selectClassArray = e.detail.data;
-				setClasses(selectClassArray);
-			})
-			//删除班级的监听
-		setRemoveClassListener();
-		//设置是否在线的监听
-		setIsOnline();
-		//提交作业的监听
-		setSubmitEvent();
-		var lastEditRange = null;
-		var publish_container = document.getElementById('publish-content');
-		//录音键
-		events.addTap('getRecord', function() {
-//				startRecord()
-				mui.toast('功能暂未开放！');
-			})
-			//相机按钮
-		events.addTap('getImg', function() {
-				if(CloudFileUtil.files.length < 9) {
-					camera.getPic(camera.getCamera(), function(picPath) {
-						plus.nativeUI.showWaiting(storageKeyName.WAITING);
-						var saveSpace = storageKeyName.CLASSSPACE; //保存空间
-						compress.compressPIC(picPath, function(event) {
-							var localPath = event.target;
-							var data = CloudFileUtil.getSingleUploadDataOptions(localPath, 6, 200, 0, saveSpace);
-							CloudFileUtil.getQNUpTokenWithManage(storageKeyName.QNGETUPLOADTOKEN, data.options, function(datas) {
-								console.log("获取的数据：" + JSON.stringify(datas));
-								if(datas.Status == 1) {
-									var tokenInfo = datas.Data;
-
-									//上传文件
-									CloudFileUtil.uploadFile(tokenInfo, localPath, function(uploadData, status) {
-										console.log(JSON.stringify(uploadData));
-										var img = { //图片信息
-											url: tokenInfo.Domain + tokenInfo.Key,
-											thumb: tokenInfo.OtherKey[data.thumbKey],
-											type: 1
-										}
-										//关闭等待框
-										plus.nativeUI.closeWaiting();
-										//放置图片
-					 					CloudFileUtil.setPic(img);
-									});
-								}
-
-							}, function(xhr, type, errorThrown) {
-								console.log("错误类型：" + type + errorThrown);
-								plus.nativeUI.closeWaiting(); //关闭等待框
-							});
-						})
-					})
-				} else {
-					mui.toast('上传图片附件不得多于9张！');
-				}
-			})
-			//录像按钮
-		events.addTap('getVideo', function() {
-//			camera.getVideo(camera.getCamera(), function(videoPath) {
-//				console.log("videoPath:" + videoPath);
-//			})
-			mui.toast('功能暂未开放！');
-		});
+	events.preload('classes-select.html', 200);
+	window.addEventListener('postClasses', function(e) {
+		CloudFileUtil.files = [];
+		personalUTID = parseInt(window.myStorage.getItem(window.storageKeyName.PERSONALINFO).utid);
+		console.log('发布作业界面获取的班级数据：' + JSON.stringify(e.detail.data));
+		var switchItem = document.getElementById("onlineSwitch");
+		var isActive = switchItem.classList.contains("mui-active");
+		if(!isActive) {
+			mui('#onlineSwitch').switch().toggle();
+		}
+		document.getElementById('publish-content').value = null;
+		//选中班级为全部班级
+		selectClassArray = e.detail.data;
+		//请求所有班级学生数据
+		requestClassStudents();
+		//科目
+		requestSubjectList(setSubjects);
 	})
-	//17.获取所有科目列表
+	events.addTap('select-classes', function() {
+		events.fireToPageWithData('classes-select.html', 'postClasses', selectClassArray);
+	})
+	CloudFileUtil.setDelPicListener();
+	/**
+	 * 监听选择班级后的返回数据
+	 */
+	window.addEventListener('selectedClasses', function(e) {
+		selectClassArray = e.detail.data;
+		setClasses(selectClassArray);
+	})
+	//删除班级的监听
+	setRemoveClassListener();
+	//设置是否在线的监听
+	setIsOnline();
+	//提交作业的监听
+	setSubmitEvent();
+	var lastEditRange = null;
+	var publish_container = document.getElementById('publish-content');
+	//录音键
+	//		events.addTap('getRecord', function() {
+	////				startRecord()
+	//				mui.toast('功能暂未开放！');
+	//			})
+	//相册按钮
+	events.addTap('get_gallery', function() {
+		if(CloudFileUtil.files.length < 9) {
+			var picCount = 0; //上传图片计数
+			gallery.getMultiplePic(9 - CloudFileUtil.files.length, function(paths) { //回调函数
+				plus.nativeUI.showWaiting(storageKeyName.UPLOADING); //等待框
+				console.log("保存的路径：" + JSON.stringify(paths));
+				var saveSpace = storageKeyName.CLASSSPACE; //保存路径
+				compress.compressPics(paths, function(compressedPaths) {
+					console.log('获取的图片路径：' + JSON.stringify(compressedPaths));
+					var data = CloudFileUtil.getMultipleUploadDataOptions(compressedPaths, 6, 200, 0, saveSpace); //获取获取token的各参数
+					CloudFileUtil.getQNUpTokenWithManage(storageKeyName.QNGETUPLOADTOKEN, data.options, function(datas) {
+							console.log("传回来的值：" + JSON.stringify(datas)) //回调数据
+							if(datas.Status == 1) { //成功
+								var tokenInfos = datas.Data; //参数信息
+								//上传图片
+								CloudFileUtil.uploadFiles(compressedPaths, tokenInfos, function(uploadData, status) {
+									console.log(JSON.stringify(uploadData));
+									var img = {
+										url: tokenInfos[0].Domain + JSON.parse(uploadData.responseText).key,
+										thumb: (tokenInfos[0].Domain + JSON.parse(uploadData.responseText).key).replace(saveSpace, saveSpace + "thumb/"),
+										type: 1
+									}
+									picCount++;
+									CloudFileUtil.setPic(img); //放置图片
+									if(picCount == compressedPaths.length) { //所有图片已上传
+										plus.nativeUI.closeWaiting(); //关闭等待框
+									}
+								});
+
+							}
+						},
+						//错误的回调
+						function(xhr, type, errorThrown) {
+							console.log("错误类型：" + type + errorThrown);
+							plus.nativeUI.closeWaiting();
+						});
+				});
+
+			});
+		} else {
+			mui.toast('上传图片附件不得多于9张！');
+		}
+	});
+}
+//相机按钮
+events.addTap('getImg', function() {
+	if(CloudFileUtil.files.length < 9) {
+		camera.getPic(camera.getCamera(), function(picPath) {
+			plus.nativeUI.showWaiting(storageKeyName.WAITING);
+			var saveSpace = storageKeyName.CLASSSPACE; //保存空间
+			compress.compressPIC(picPath, function(event) {
+				var localPath = event.target;
+				var data = CloudFileUtil.getSingleUploadDataOptions(localPath, 6, 200, 0, saveSpace);
+				CloudFileUtil.getQNUpTokenWithManage(storageKeyName.QNGETUPLOADTOKEN, data.options, function(datas) {
+					console.log("获取的数据：" + JSON.stringify(datas));
+					if(datas.Status == 1) {
+						var tokenInfo = datas.Data;
+
+						//上传文件
+						CloudFileUtil.uploadFile(tokenInfo, localPath, function(uploadData, status) {
+							console.log(JSON.stringify(uploadData));
+							var img = { //图片信息
+								url: tokenInfo.Domain + tokenInfo.Key,
+								thumb: tokenInfo.OtherKey[data.thumbKey],
+								type: 1
+							}
+							//关闭等待框
+							plus.nativeUI.closeWaiting();
+							//放置图片
+							CloudFileUtil.setPic(img);
+						});
+					}
+
+				}, function(xhr, type, errorThrown) {
+					console.log("错误类型：" + type + errorThrown);
+					plus.nativeUI.closeWaiting(); //关闭等待框
+				});
+			})
+		})
+	} else {
+		mui.toast('上传图片附件不得多于9张！');
+	}
+})
+//录像按钮
+events.addTap('getVideo', function() {
+	//			camera.getVideo(camera.getCamera(), function(videoPath) {
+	//				console.log("videoPath:" + videoPath);
+	//			})
+	mui.toast('功能暂未开放！');
+});
+})
+//17.获取所有科目列表
 function requestSubjectList(callback) {
 	//所需参数
 	var comData = {};
@@ -121,71 +167,71 @@ function requestSubjectList(callback) {
  * @param {Object} subjectList
  */
 var setSubjects = function(subjectList) {
-		events.clearChild(subjectsContainer);
-		subjectList.forEach(function(subject, i) {
-			var op = document.createElement('option');
-			op.className="select-subject";
-			op.value = subject.Value;
-			op.innerText = subject.Text;
-			subjectsContainer.appendChild(op);
-		});
-	}
-	//	/**
-	//	 * 选中科目的监听
-	//	 */
-	//var getSelectSubject = function() {
-	//		selectSubjectID = subjectsContainer[subjectsContainer.selectedIndex].value;
-	//	}
-	/**
-	 * 放置班级列表
-	 * @param {Object} classes
-	 */
+	events.clearChild(subjectsContainer);
+	subjectList.forEach(function(subject, i) {
+		var op = document.createElement('option');
+		op.className = "select-subject";
+		op.value = subject.Value;
+		op.innerText = subject.Text;
+		subjectsContainer.appendChild(op);
+	});
+}
+//	/**
+//	 * 选中科目的监听
+//	 */
+//var getSelectSubject = function() {
+//		selectSubjectID = subjectsContainer[subjectsContainer.selectedIndex].value;
+//	}
+/**
+ * 放置班级列表
+ * @param {Object} classes
+ */
 var setClasses = function() {
-		//班级数据列表控件
-		var classesContainer = document.getElementById('classes');
-		//清空班级数据
-		events.clearChild(classesContainer)
-		for(var i in selectClassArray) {
-			if(selectClassArray[i].isSelected) {
-				var p = document.createElement('p');
-				p.className = 'class-container gid' + selectClassArray[i].gid;
-				//				p.innerText = classes[i].gname;
-				p.innerHTML = selectClassArray[i].gname + '<span class="iconfont icon-close class-del"></span>'
-				classesContainer.appendChild(p);
-				p.querySelector('.class-del').bindClass = selectClassArray[i];
-			}
-
+	//班级数据列表控件
+	var classesContainer = document.getElementById('classes');
+	//清空班级数据
+	events.clearChild(classesContainer)
+	for(var i in selectClassArray) {
+		if(selectClassArray[i].isSelected) {
+			var p = document.createElement('p');
+			p.className = 'class-container gid' + selectClassArray[i].gid;
+			//				p.innerText = classes[i].gname;
+			p.innerHTML = selectClassArray[i].gname + '<span class="iconfont icon-close class-del"></span>'
+			classesContainer.appendChild(p);
+			p.querySelector('.class-del').bindClass = selectClassArray[i];
 		}
+
 	}
-	/**
-	 * 删除班级的监听
-	 */
+}
+/**
+ * 删除班级的监听
+ */
 var setRemoveClassListener = function() {
-		mui('.receive-classes').on('tap', '.class-del', function() {
-			var classes = document.getElementById('classes');
-			classes.removeChild(classes.querySelector('.gid' + this.bindClass.gid));
-			console.log('删除的班级数据：' + JSON.stringify(this.bindClass));
-			selectClassArray[selectClassArray.indexOf(this.bindClass)].isSelected = false;
-			console.log('删除班级后所有班级数据：' + JSON.stringify(selectClassArray));
-		})
-	}
-	/**
-	 * 是否在线提交
-	 */
+	mui('.receive-classes').on('tap', '.class-del', function() {
+		var classes = document.getElementById('classes');
+		classes.removeChild(classes.querySelector('.gid' + this.bindClass.gid));
+		console.log('删除的班级数据：' + JSON.stringify(this.bindClass));
+		selectClassArray[selectClassArray.indexOf(this.bindClass)].isSelected = false;
+		console.log('删除班级后所有班级数据：' + JSON.stringify(selectClassArray));
+	})
+}
+/**
+ * 是否在线提交
+ */
 var setIsOnline = function() {
-		document.getElementById("onlineSwitch").addEventListener("toggle", function(event) {
-			if(event.detail.isActive) {
-				submitOnLine = true;
-				console.log("你启动了开关");
-			} else {
-				submitOnLine = false;
-				console.log("你关闭了开关");
-			}
-		})
-	}
-	/**
-	 * 提交按钮的监听
-	 */
+	document.getElementById("onlineSwitch").addEventListener("toggle", function(event) {
+		if(event.detail.isActive) {
+			submitOnLine = true;
+			console.log("你启动了开关");
+		} else {
+			submitOnLine = false;
+			console.log("你关闭了开关");
+		}
+	})
+}
+/**
+ * 提交按钮的监听
+ */
 var setSubmitEvent = function() {
 	//提交按钮
 	events.addTap('submitBtn', function() {
@@ -292,8 +338,8 @@ function requestPublishHomework() {
 	}
 	//给学生去重
 	tempStuArray = arrayDupRemoval(tempStuArray);
-	for(var i in CloudFileUtil.files){
-		CloudFileUtil.files[i].order=i;
+	for(var i in CloudFileUtil.files) {
+		CloudFileUtil.files[i].order = i;
 	}
 	//所需参数
 	var comData = {
@@ -302,7 +348,7 @@ function requestPublishHomework() {
 		studentIds: tempStuArray.toString(), //班级Id+学生Id串，班级Id和学生Id以“|“分割，如“班级Id|学生Id”，每对id之间逗号分隔，例如“1|1,1|2”；
 		content: document.getElementById('publish-content').value, //作业内容
 		submitOnLine: submitOnLine, //是否需要在线提交；
-		files:CloudFileUtil.files  //上传文件的id串，例如“1,2”；
+		files: CloudFileUtil.files //上传文件的id串，例如“1,2”；
 	};
 	// 等待的对话框
 	var wd = plus.nativeUI.showWaiting(storageKeyName.WAITING);
@@ -311,7 +357,7 @@ function requestPublishHomework() {
 		wd.close();
 		console.log('发布作业界面发布作业回调：' + JSON.stringify(data));
 		if(data.RspCode == 0) {
-			CloudFileUtil.files=[];
+			CloudFileUtil.files = [];
 			events.clearChild(document.getElementById('pictures'));
 			//提示成功，清空界面数据
 			document.getElementById('publish-content').value = '';
