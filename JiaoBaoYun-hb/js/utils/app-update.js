bf77ebbf52662c1df9e6d31002baf792881cfd42
rockan007 <rockan007@gmail.com>
2017-03-02 15:55:40 +0800

    升级逻辑修改

--- JiaoBaoYun-hb/js/utils/app-update.js
+++ JiaoBaoYun-hb/js/utils/app-update.js
@@ -11,6 +11,7 @@ var appUpdate = (function(mod) {
 				mod.appVersion = getBigVersion(inf.version,plus.runtime.version);
 				console.log('应用版本号:'+plus.runtime.version+',资源升级版本号:'+inf.version)
 				console.log("当前应用版本：" + mod.appVersion);
+				mui.toast('应用版本号:'+plus.runtime.version+',资源升级版本号:'+inf.version)
 				getUpCondition(versionInfo); //判断是否更新
 			});
 		}
@@ -27,6 +28,8 @@ var appUpdate = (function(mod) {
 				return version0;
 			}else if(parseInt(version0Array[i])<parseInt(version1Array[i])){
 				return version1;
+			}else{
+				return version0;
 			}
 		}
 	}
@@ -77,8 +80,10 @@ var appUpdate = (function(mod) {
 			for(var i in numArray) {
 				if(i == 0) {
 					minMax.max = parseInt(numArray[i]);
-				} else {
+				} else if(i<3) {
 					min+=numArray[i];
+				}else{
+					break;
 				}
 			}
 			minMax.min=parseInt(min);
