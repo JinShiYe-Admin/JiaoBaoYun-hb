33091baf1695aad0bffd3129d1ae5f02f6942e88
BugHunter1234 <444811716@qq.com>
2017-03-07 17:43:12 +0800

    调整获取上传的token

--- JiaoBaoYun-hb/js/utils/CloudFileUtil.js
+++ JiaoBaoYun-hb/js/utils/CloudFileUtil.js
@@ -80,7 +80,7 @@ var CloudFileUtil = (function($, mod) {
 					case 6: //家校圈
 						desKey = "jxq789!@";
 						break;
-					case 7: //家校圈
+					case 7: //求知
 						desKey = "qz123qwe";
 						break;
 					default:
@@ -168,23 +168,24 @@ var CloudFileUtil = (function($, mod) {
 	}
 
 	/**
-	 * 获取上传到七牛的uptoken（单个文件的token）
-	 * 使用到的地方：1.个人头像2.资料头像3.群头像4.上传到云存储
+	 * 获取上传到七牛的uptoken（单/多个文件的uptoken）
+	 * 使用到的地方：1.个人头像2.资料头像3.群头像4.上传到云存储5.记事
 	 * @author 莫尚霖
 	 * @param {Object} url 获取token的url
 	 * @param {Object} data 数据 json
 	 * @param {Object} successCB 成功的回调successCB(data)
 	 * @param {Object} errorCB 失败的回调errorCB(xhr, type, errorThrown);
 	 * data={
-	 * 	type:'',//str 必填 获取上传token的类型。0上传需要生成缩略图的文件；1上传文件
+	 * 	type:'',//str 必填 获取上传token的类型。0上传需要生成缩略图的文件；1上传文件；2上传需要生成缩略图的多个文件
 	 *  QNFileName:'',//str 必填 存放到七牛的文件名
+	 * 	fileArray:[],//array 选填  type为2时有效，多个文件的路径
 	 *  appId:'' , //int 必填 项目id
 	 *  mainSpace:'', //str 必填 文件存放在私有空间或公有空间
 	 *  uploadSpace: '',//str 必填  上传的空间（文件二级文件名）
 	 *  imageThumb:'',//str json 选填 type为0时有效，缩略图存放在私有空间或公有空间，默认mainSpace
-	 *  style:{ 	//json 选填 type为0时有效，配置生成缩略图的最大宽和高
-	 * 		maxWidth:'', //int 选填 配置生成缩略图的最大宽,默认100
-	 *  	maxHeight:'' //int 选填 配置生成缩略图的最大高，默认100
+	 *  style:{ 	//json 选填 type为0，2时有效，配置生成缩略图的最大宽和高
+	 * 		maxWidth:'', //int 选填 配置生成缩略图的最大宽,默认200
+	 *  	maxHeight:'' //int 选填 配置生成缩略图的最大高，默认200
 	 *  }
 	 * }
 	 */
@@ -192,18 +193,19 @@ var CloudFileUtil = (function($, mod) {
 		console.log('getQNUpToken ' + url + ' ' + JSON.stringify(data));
 		var type = ''; //获取上传token的类型。0上传需要生成缩略图的文件；1上传文件
 		var QNFileName = ''; //存放到七牛的文件名
+		var fileList = []; //上传文件的路径
 		var desKey = ''; //项目名称
 		var appId = 0; //项目id
 		var mainSpace = ''; //文件存放在私有空间或公有空间
 		var imageThumb = ''; //缩略图存放在私有空间或公有空间
 		var saveSpace = ''; //上传的空间
 		var configure = {}; //配置的数据
-		var maxWidth = '100'; //type为0时 缩略图默认宽为100
-		var maxHeight = '100'; //type为0时 缩略图默认高为100
+		var maxWidth = '200'; //type为0时 缩略图默认宽为200
+		var maxHeight = '200'; //type为0时 缩略图默认高为200
 		if(data) {
 			if(data.type) {
 				type = data.type
-				if(type == 0) {
+				if(type == 0 || type == 2) {
 					if(data.style) {
 						if(data.style.maxWidth) {
 							maxWidth = data.style.maxWidth
@@ -211,6 +213,11 @@ var CloudFileUtil = (function($, mod) {
 						if(data.style.maxHeight) {
 							maxHeight = data.style.maxHeight
 						}
+					}
+				}
+				if(type == 2) {
+					if(data.fileArray) {
+						fileList = data.fileArray;
 					}
 				}
 			}
@@ -239,7 +246,7 @@ var CloudFileUtil = (function($, mod) {
 					case 6: //家校圈
 						desKey = "jxq789!@";
 						break;
-					case 7: //家校圈
+					case 7: //求知
 						desKey = "qz123qwe";
 						break;
 					default:
@@ -258,43 +265,77 @@ var CloudFileUtil = (function($, mod) {
 			if(data.uploadSpace) {
 				saveSpace = data.uploadSpace;
 			}
-
 		}
-		if((type == '0' || type == '1') && QNFileName != '' && desKey != '' && mainSpace != '' && saveSpace != '') {
+		if((type == '0' || type == '1' || type == '2') && desKey != '' && mainSpace != '' && saveSpace != '') {
 			var thumbSpace = ''; //缩略图的七牛空间
 			var ops = '' //七牛预持久化命令
-			if(type == '0') {
-				thumbSpace = saveSpace + 'Thumb/'; //缩略图的七牛空间
-				var temp = QNFileName.split('.');
-				console.log(JSON.stringify(temp));
-				var thumbName = temp[0];
-				var thumbType = temp[1];
-				if(thumbType == 'avi' || thumbType == 'mp4' || thumbType == 'flv' || thumbType == 'swf' || thumbType == '3gp' || thumbType == 'rm') {
-					//视频
-					configure.thumbKey = Qiniu.URLSafeBase64Encode(imageThumb + ":" + thumbSpace + thumbName + '.png');
-					ops = "vframe/png/offset/1/w/" + maxWidth + "/h/" + maxHeight + "|saveas/" + configure.thumbKey;
-				} else {
-					//图片
-					if(appId == 5) { //头像
-						configure.thumbKey = Qiniu.URLSafeBase64Encode(imageThumb + ":" + thumbSpace + QNFileName);
-					} else if(appId == 4) { //云存储
+			if(type == '0' || type == '1') {
+				if(type == '0') {
+					thumbSpace = saveSpace + 'Thumb/'; //缩略图的七牛空间
+					var temp = QNFileName.split('.');
+					console.log(JSON.stringify(temp));
+					var thumbName = temp[0];
+					var thumbType = temp[1];
+					if(thumbType == 'avi' || thumbType == 'mp4' || thumbType == 'flv' || thumbType == 'swf' || thumbType == '3gp' || thumbType == 'rm') {
+						//视频
 						configure.thumbKey = Qiniu.URLSafeBase64Encode(imageThumb + ":" + thumbSpace + thumbName + '.png');
+						ops = "vframe/png/offset/1/w/" + maxWidth + "/h/" + maxHeight + "|saveas/" + configure.thumbKey;
+					} else {
+						//图片
+						if(appId == 5) { //头像
+							configure.thumbKey = Qiniu.URLSafeBase64Encode(imageThumb + ":" + thumbSpace + QNFileName);
+						} else if(appId == 4) { //云存储
+							configure.thumbKey = Qiniu.URLSafeBase64Encode(imageThumb + ":" + thumbSpace + thumbName + '.png');
+						}
+						ops = "imageView2/2/w/" + maxWidth + "/h/" + maxHeight + "/format/png|saveas/" + configure.thumbKey;
 					}
-					ops = "imageView2/2/w/" + maxWidth + "/h/" + maxHeight + "/format/png|saveas/" + configure.thumbKey;
+				}
+
+				var param = {
+					Bucket: mainSpace,
+					Key: saveSpace + QNFileName,
+					Pops: ops,
+					NotifyUrl: ''
+				}
+				console.log("参数数据：" + JSON.stringify(param));
+
+				configure.options = {
+					AppID: appId,
+					Param: encryptByDES(desKey, JSON.stringify(param))
+				}
+			} else if(type == '2') {
+				var params = [];
+				var uploadOptions = { //上传七牛后的处理参数
+					type: 0, //处理类型 0：缩略图 1 裁剪 10 缩略图+裁剪
+					thumbSize: {
+						width: maxWidth, //缩略图最大宽度
+						height: maxHeight //缩略图最大高度
+					}
+				}
+
+				for(var i = 0; i < fileList.length; i++) {
+					var QNFileName; //文件名
+					var param = {};
+					param.Bucket = mainSpace;
+					//获取文件路径
+					var filePaths = fileList[i].split("/");
+					QNFileName = filePaths[filePaths.length - 1];
+					param.Key = saveSpace + QNFileName;
+					console.log('key:' + param.Key);
+					//获取处理参数
+					var opsData = getOptions(uploadOptions, saveSpace, mainSpace, QNFileName);
+					param.Pops = opsData.ops;
+					param.NotifyUrl = '';
+					//保存空间值
+					params.push(param);
+				}
+
+				configure.options = {
+					AppID: appId,
+					Param: encryptByDES(desKey, JSON.stringify(params))
 				}
 			}
 
-			var param = {
-				Bucket: mainSpace,
-				Key: saveSpace + QNFileName,
-				Pops: ops,
-				NotifyUrl: ''
-			}
-			console.log("参数数据：" + JSON.stringify(param))
-			configure.options = {
-				AppID: appId,
-				Param: encryptByDES(desKey, JSON.stringify(param))
-			}
 			console.log("参数数据：" + JSON.stringify(configure.options))
 			//获取token
 			mod.getQNUpTokenWithManage(url, configure.options, function(data) {
@@ -427,7 +468,7 @@ var CloudFileUtil = (function($, mod) {
 		return data;
 	}
 	/**
-	 * 
+	 *
 	 * @param {Object} manageOptions 处理参数
 	 * @param {Object} saveSpace 保存空间
 	 * @param {Object} mainSpace 主空间
@@ -486,13 +527,13 @@ var CloudFileUtil = (function($, mod) {
 		} else {
 			mainSpace = storageKeyName.QNPUBSPACE; //七牛公共空间
 		}
-		var QNFileName;//文件名
+		var QNFileName; //文件名
 		var params = [];
 		for(var i in picPaths) {
-			if(parseInt(i)==0){
-				uploadOptions.type=10;
-			}else{
-				uploadOptions.type=0;
+			if(parseInt(i) == 0) {
+				uploadOptions.type = 10;
+			} else {
+				uploadOptions.type = 0;
 			}
 			var param = {};
 			param.Bucket = mainSpace;
@@ -520,7 +561,7 @@ var CloudFileUtil = (function($, mod) {
 		return option ? option : '';
 	}
 	/**
-	 * 
+	 *
 	 * @param {Object} appId app的id
 	 */
 	var getAppKey = function(appId) {
