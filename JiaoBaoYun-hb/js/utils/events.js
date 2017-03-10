cd6afeebf5248997fecb45b60d690ee6c06be2a7
sam <349708436@qq.com>
2017-03-10 17:35:05 +0800

    更新
    更新3月10日QQ问题 仅剩 垂直居中及上拉js错误未解决

--- JiaoBaoYun-hb/js/utils/events.js
+++ JiaoBaoYun-hb/js/utils/events.js
@@ -462,6 +462,27 @@ var events = (function(mod) {
 			}
 		})
 	}
+
+	mod.format = function(dateTime,format) {
+		var o = {
+			"M+": dateTime.getMonth() + 1, //month  
+			"d+": dateTime.getDate(), //day  
+			"h+": dateTime.getHours(), //hour  
+			"m+": dateTime.getMinutes(), //minute  
+			"s+": dateTime.getSeconds(), //second  
+			"q+": Math.floor((dateTime.getMonth() + 3) / 3), //quarter  
+			"S": dateTime.getMilliseconds() //millisecond  
+		};
+		if(/(y+)/.test(format)) {
+			format = format.replace(RegExp.$1, (dateTime.getFullYear() + "").substr(4 - RegExp.$1.length));
+		}
+		for(var k in o) {
+			if(new RegExp("(" + k + ")").test(format)) {
+				format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+			}
+		}
+		return format;
+	}
 	return mod;
 
 })(events || {});
