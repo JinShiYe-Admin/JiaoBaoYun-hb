a20aa6f0e89981ca4c973f473e595f58dde90a94
rockan007 <rockan007@gmail.com>
2017-03-20 10:21:00 +0800

    如果是老师和家长双重身份，退出作业的时候在学生作业列表，重新进入作业作业也会重复
    匿名文字过大

--- JiaoBaoYun-hb/js/homework/homework.js
+++ JiaoBaoYun-hb/js/homework/homework.js
@@ -1,6 +1,6 @@
 var teacherClasses = []; //老师身份关联班级
 var studentClasses = []; //学生身份关联班级
-var role = 2;//老师角色 30为家长+学生角色
+var role = 2; //老师角色 30为家长+学生角色
 //加载子页面
 events.initSubPage('homework-tea-sub.html');
 mui.plusReady(function() {
@@ -10,20 +10,20 @@ mui.plusReady(function() {
 	var title = document.getElementById('workPage-title');
 	var roles = document.getElementById('workPage-roles');
 	var btn_more = document.getElementById('more');
-	btn_more.style.display='none';
+	btn_more.style.display = 'none';
 	window.addEventListener('postClasses', function(e) {
-			var data = e.detail.data;
-			console.log('作业主界面获取信息：' + JSON.stringify(e.detail.data));
-			studentClasses = data.studentClasses;
-			teacherClasses = data.teacherClasses;
-			setChoices(title, roles, btn_more);
-		})
-		//三道杠的点击事件
-//	events.addTap('more', function() {
-//			//通知子页面，显示、关闭菜单 
-//			events.fireToPageNone('homework-tea-sub.html', 'togglePop');
-//		})
-		//相机点击事件
+		var data = e.detail.data;
+		console.log('作业主界面获取信息：' + JSON.stringify(e.detail.data));
+		studentClasses = data.studentClasses;
+		teacherClasses = data.teacherClasses;
+		setChoices(title, roles, btn_more);
+	})
+	//三道杠的点击事件
+	//	events.addTap('more', function() {
+	//			//通知子页面，显示、关闭菜单 
+	//			events.fireToPageNone('homework-tea-sub.html', 'togglePop');
+	//		})
+	//相机点击事件
 	events.addTap('icon-camero', function() {
 		events.fireToPageWithData('publish-answer.html', 'roleInfo', {
 			role: role,
@@ -31,22 +31,28 @@ mui.plusReady(function() {
 			teacherClasses: teacherClasses
 		})
 	})
+	
 	//角色选择的监听
 	roles.addEventListener("toggle", function(event) {
 		if(event.detail.isActive) {
-			role=30;
-//			btn_more.style.display='block';
+			role = 30;
+			//			btn_more.style.display='block';
 		} else {
-			role=2;
-//			btn_more.style.display='none';
+			role = 2;
+			//			btn_more.style.display='none';
 		}
 		events.fireToPageNone('homework-tea-sub.html', 'roleChanged', role)
 	})
-	var _back=mui.back;
-	mui.back=function(){
-		if(plus.webview.currentWebview().opener().id="homework-commented.html"){
+	var _back = mui.back;
+	mui.back = function() {
+		if(plus.webview.currentWebview().opener().id = "homework-commented.html") {
+			if(teacherClasses.length > 0 && studentClasses.length > 0) {
+				if(roles.classList.contains("mui-active")) {
+					mui('#workPage-roles').switch().toggle();
+				}
+			}
 			plus.webview.getWebviewById('index.html').show();
-		}else{
+		} else {
 			_back();
 		}
 	}
@@ -67,12 +73,12 @@ var sendMsgToSub = function() {
  */
 var setChoices = function(title, roles, btn_m) {
 	if(teacherClasses.length > 0 && studentClasses.length > 0) {
-		if(roles.classList.contains("mui-active")){
+		if(roles.classList.contains("mui-active")) {
 			mui('#workPage-roles').switch().toggle();
 		}
 		title.style.display = 'none';
 		roles.style.display = 'inline-block';
-//		btn_m.style.display = 'none';
+		//		btn_m.style.display = 'none';
 		role = 2;
 
 	} else if(teacherClasses.length > 0) {
@@ -80,12 +86,12 @@ var setChoices = function(title, roles, btn_m) {
 		roles.style.display = 'none';
 		title.innerText = '我发布的';
 		role = 2;
-//		btn_m.style.display = 'none';
+		//		btn_m.style.display = 'none';
 	} else if(studentClasses.length > 0) {
 		title.style.display = 'inline-block';
 		roles.style.display = 'none';
 		title.innerText = '学生作业'
-//		btn_m.style.display = 'block'
+		//		btn_m.style.display = 'block'
 		role = 30;
 	}
 	//向子页面传递数据
