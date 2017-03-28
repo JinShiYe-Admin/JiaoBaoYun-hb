fbda1b8289abc4251aa09dd72e08a2952fe2fbae
BugHunter1234 <444811716@qq.com>
2017-03-28 09:02:29 +0800

    增加安卓调用原生的方法打开文件

--- JiaoBaoYun-hb/js/utils/playutil.js
+++ JiaoBaoYun-hb/js/utils/playutil.js
@@ -1,5 +1,5 @@
 /**
- * 播放视频
+ * 打开文件
  */
 var playutil = (function(mod) {
 
@@ -52,10 +52,124 @@ var playutil = (function(mod) {
 			var intent = new Intent(Intent.ACTION_VIEW);
 			var uri = Uri.parse(videoUrl);
 			intent.setDataAndType(uri, "video/*");
-			activity.startActivity(intent);
+			try {
+				activity.startActivity(intent);
+			} catch(e) {
+				alert('### ERROR ### 安卓调用应用 name:' + e.name + " message:" + e.message);
+			}
 		} else {
 			console.log('### ERROR ### ' + plus.os.name);
 		}
+	}
+
+	/**
+	 * 打开某个文件
+	 * @param {Object} fpath 文件路径
+	 * @param {Object} errorCallback 失败的回调
+	 */
+	mod.openFile = function(fpath, errorCallback) {
+		try {
+			plus.runtime.openFile(fpath, '', function(error) {
+				console.log('打开文件失败:' + JSON.stringify(error));
+				if(plus.os.name == 'Android') {
+					mod.openFileAndroid(fpath, function(text) {
+						errorCallback(text);
+					});
+				} else {
+					errorCallback('打开文件失败 调用第三方程序失败');
+				}
+			});
+		} catch(e) {
+			errorCallback('### ERROR ### H5+打开文件 name:' + e.name + " message:" + e.message);
+		}
+	}
+
+	/**
+	 * 安卓打开某个应用
+	 * @param {Object} fpath 文件路径
+	 * @param {Object} errorCallback 失败的回调
+	 */
+	mod.openFileAndroid = function(fpath, errorCallback) {
+		if(plus.os.name == 'Android') {
+			var Intent = plus.android.importClass("android.content.Intent");
+			var Uri = plus.android.importClass("android.net.Uri");
+			var activity = plus.android.runtimeMainActivity();
+			var intent = new Intent(Intent.ACTION_VIEW);
+			var uri = Uri.parse(fpath);
+			var type = mod.MIMETypeAndroid(fpath);
+			intent.setDataAndType(uri, type);
+			try {
+				activity.startActivity(intent);
+			} catch(e) {
+				errorCallback('### ERROR ### 安卓调用应用 name:' + e.name + " message:" + e.message);
+			}
+		} else {
+			console.log('### ERROR ### ' + plus.os.name);
+		}
+	}
+
+	/**
+	 * 返回 MIME Type
+	 * @param {Object} fpath
+	 */
+	mod.MIMETypeAndroid = function(fpath) {
+		//console.log('MIMEType:' + fpath);
+		var MIMEType = '*/*';
+		var nameList = fpath.split(".");
+		var type = nameList[nameList.length - 1];
+		type = type.toLowerCase(); //转换为小写
+		//console.log('MIMEType type ' + type);
+		switch(type) {
+			case 'avi': //视频类型
+			case 'mp4':
+			case 'flv':
+			case 'swf':
+			case '3gp':
+			case 'rm':
+			case 'wma':
+			case 'asf':
+			case 'wmv':
+			case 'rmvb':
+				MIMEType = 'video/*';
+				break;
+			case 'psd': //图片类型
+			case 'jpeg':
+			case 'jpg':
+			case 'png':
+			case 'gif':
+			case 'webp':
+			case 'tiff':
+			case 'bmp':
+			case 'cod':
+			case 'cal':
+			case 'dcx':
+			case 'eri':
+			case 'jpe':
+			case 'jpz':
+				MIMEType = 'image/*';
+				break;
+			case 'cda': //音频类型
+			case 'wav':
+			case 'cda':
+			case 'aif':
+			case 'aiff':
+			case 'au':
+			case 'mp1':
+			case 'mp2':
+			case 'mp3':
+			case 'ra':
+			case 'rm':
+			case 'ram':
+			case 'mid':
+			case 'Rmi':
+				MIMEType = 'audio/*';
+				break;
+			default:
+				console.log('MIME-Type 不匹配');
+				break;
+		}
+		//console.log('MIMEType ' + type);
+		return MIMEType;
 	}
 
 	return mod;
