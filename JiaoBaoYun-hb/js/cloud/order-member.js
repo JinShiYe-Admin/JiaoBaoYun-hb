e4b55319e474f0852831ec73877b709c2666999e
rockan007 <rockan007@gmail.com>
2017-05-03 11:14:12 +0800

    支付

--- JiaoBaoYun-hb/js/cloud/order-member.js
+++ JiaoBaoYun-hb/js/cloud/order-member.js
@@ -1,17 +1,24 @@
 mui.init();
 mui(".mui-scroll-wrapper").scroll();
 mui.plusReady(function(){
+	setData();
 	setListeners();
 })
+function setData(){
+	
+}
 function setListeners(){
 	mui(".mui-slider-item.mui-control-content").on("tap",".deal-button",function(){
 		this.disabled=true;
-		var type=document.querySelector(".mui-slider-indicator.mui-segmented-control").querySelector(".mui-active").value;
-		console.log("订购的会员类型："+type)
-		events.singleWebviewInPeriod(this,"member-pay.html",{
-			memberName:'普通会员',
-			memberTime:'3个月',
-			memberPay:'12'
+		var item=this;
+		var type=parseInt(document.querySelector(".mui-slider-indicator.mui-segmented-control").querySelector(".mui-active").getAttribute("value"));
+		console.log("订购的会员类型："+type);
+		var duration=item.parentElement.querySelector(".duration-info").innerText;
+		var price=item.parentElement.querySelector(".price-info").innerText;
+		events.singleWebviewInPeriod(item,"member-pay.html",{
+			memberName:type,
+			memberTime:duration,
+			memberPay:price
 		})
 	})
 }
