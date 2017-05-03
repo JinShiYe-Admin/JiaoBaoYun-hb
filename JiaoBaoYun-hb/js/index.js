27a4617e1870377c830437a9e734e06c385e897e
Zqw <578159259@qq.com>
2017-05-03 12:03:39 +0800

    更新

--- JiaoBaoYun-hb/js/index.js
+++ JiaoBaoYun-hb/js/index.js
@@ -394,6 +394,10 @@ var setActivePage = function() {
 	var splitActiveTabs = activeTab.split("/");
 	var activeId = splitActiveTabs[splitActiveTabs.length - 1];
 	console.log("要显示的界面：" + activeTab);
-	plus.webview.show(activeId, "fade-in", 300);
+	if(mui.os.ios){
+		plus.webview.show(activeId);
+	}else{
+		plus.webview.show(activeId, "fade-in", 300);
+	}
 	//	changRightIcons(activeTab);
 }
