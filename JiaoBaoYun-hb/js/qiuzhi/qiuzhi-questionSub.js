1629c9cdbc088395de5f5b666c14a3d3f151bd63
rockan007 <rockan007@gmail.com>
2017-05-03 16:54:08 +0800

    求知问题内容换行

--- JiaoBaoYun-hb/js/qiuzhi/qiuzhi-questionSub.js
+++ JiaoBaoYun-hb/js/qiuzhi/qiuzhi-questionSub.js
@@ -621,7 +621,7 @@ function questionContent(content, flag) {
 	var height_1;
 	if(flag == 1) { //求知旧数据
 		document.getElementById("question_content").style.lineHeight = '20px';
-		document.getElementById("question_content").innerHTML = content;
+		document.getElementById("question_content").innerHTML = content.replace(/\n/g,"<br/>");
 		height_0 = document.getElementById("question_content").offsetHeight;
 		if(height_0 > 60) {
 			document.getElementById("question_content").style.lineHeight = '20px';
