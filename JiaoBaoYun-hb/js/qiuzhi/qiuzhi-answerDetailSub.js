1de9bdbdf562da65448270af7f4f32d95c769071
rockan007 <rockan007@gmail.com>
2017-05-19 06:57:06 +0800

    求知问题 求知回答 班级空间 更改刷新模块

--- JiaoBaoYun-hb/js/qiuzhi/qiuzhi-answerDetailSub.js
+++ JiaoBaoYun-hb/js/qiuzhi/qiuzhi-answerDetailSub.js
@@ -1,3 +1,50 @@
+mui.init();
+var deceleration = mui.os.ios ? 0.003 : 0.0009;
+mui('.mui-scroll-wrapper').scroll({
+	bounce: false,
+	indicators: true, //是否显示滚动条
+	deceleration: deceleration
+});
+var setFresh = function() {
+	//上拉下拉注册
+	mui(".mui-scroll-wrapper .mui-scroll").pullToRefresh({
+		down: {
+			callback: function() {
+				var self = this;
+				//清除节点
+				document.getElementById("list-container").innerHTML = "";
+				flag = 1;
+				pageIndex = 1;
+				wd = events.showWaiting();
+				requestAnswerDetail(answerInfo.AnswerId, pageIndex, 10, getInfos);
+
+				setTimeout(function() {
+					//结束下拉刷新
+					self.endPullDownToRefresh();
+					mui(".mui-pull-loading")[0].innerHTML = "上拉显示更多";
+				}, 1000);
+			}
+		},
+		up: {
+			callback: function() {
+				var self = this;
+				if(pageIndex < totalPageCount) {
+					setTimeout(function() {
+						self.endPullUpToRefresh();
+					}, 1000);
+					pageIndex++;
+					flag = 1;
+					wd = events.showWaiting();
+					requestAnswerDetail(answerInfo.AnswerId, pageIndex, 10, getInfos);
+				}else{
+					self.endPullUpToRefresh();
+					mui(".mui-pull-loading")[0].innerHTML = "没有更多了";
+				}
+			}
+		}
+	});
+}
+setFresh();
 var type = 2; //排列顺序类型1为顺序，2为倒序
 var pageIndex = 1; //当前页码
 var totalPageCount = 1; //总页数
@@ -14,29 +61,7 @@ mui.plusReady(function() {
 	mui.previewImage();
 	mui.fire(plus.webview.getWebviewById('qiuzhi-sub.html'), "answerIsReady");
 	//限制下拉刷新
-	events.limitPreviewPullDown("refreshContainer", 1);
-	//	ws=plus.webview.currentWebview();
-	//	ws.setBounce({position:"none"});
-	h5fresh.addRefresh(function() {
-		document.getElementById("list-container").innerHTML = "";
-		mui("#refreshContainer").pullRefresh().refresh(true);
-		flag = 1;
-		pageIndex = 1;
-		wd = events.showWaiting();
-		requestAnswerDetail(answerInfo.AnswerId, pageIndex, 10, getInfos);
-	}, {
-		style: "circle",
-		offset: "50px"
-	})
-	h5fresh.addPullUpFresh("#refreshContainer", function() {
-		mui('#refreshContainer').pullRefresh().endPullupToRefresh(pageIndex >= totalPageCount);
-		if(pageIndex < totalPageCount) {
-			pageIndex++;
-			flag = 1;
-			wd = events.showWaiting();
-			requestAnswerDetail(answerInfo.AnswerId, pageIndex, 10, getInfos);
-		}
-	})
+//	events.limitPreviewPullDown("refreshContainer", 1);
 	//预加载回答问题界面
 	events.preload('qiuzhi-addAnswer.html');
 
@@ -52,8 +77,8 @@ mui.plusReady(function() {
 	window.addEventListener('answerInfo', function(e) {
 		flag = 1;
 		selfId = parseInt(myStorage.getItem(storageKeyName.PERSONALINFO).utid);
-		mui('#refreshContainer').pullRefresh().enablePullupToRefresh();
-		mui('#refreshContainer').pullRefresh().refresh(true);
+//		mui('#refreshContainer').pullRefresh().enablePullupToRefresh();
+//		mui('#refreshContainer').pullRefresh().refresh(true);
 		answerData = {};
 		pageIndex = 1;
 		totalPageCount = 1;
@@ -612,7 +637,7 @@ var setCommentContainer = function(showType) {
 	if(showType) {
 		ul.style.display = "none";
 		noCom.style.display = "block";
-		mui('#refreshContainer').pullRefresh().disablePullupToRefresh();
+//		mui('#refreshContainer').pullRefresh().disablePullupToRefresh();
 	} else {
 		ul.style.display = "block";
 		noCom.style.display = "none";
