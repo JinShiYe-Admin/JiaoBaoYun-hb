87faf889daf88c8d47cf225fc5af0aeec29dad74
rockan007 <rockan007@gmail.com>
2017-05-23 11:24:40 +0800

    上拉刷新是加载图标

--- JiaoBaoYun-hb/js/mui.pullToRefresh.js
+++ JiaoBaoYun-hb/js/mui.pullToRefresh.js
@@ -34,7 +34,7 @@
 					show: true,
 					contentinit: '上拉显示更多',
 					contentdown: '上拉显示更多',
-					contentrefresh: '正在加载...',
+					contentrefresh: '<img src="../../image/utils/loading.gif" style="width:16px;vertical-align:text-bottom;"/>正在加载...',
 					contentnomore: '没有更多数据了',
 					callback: false
 				},
