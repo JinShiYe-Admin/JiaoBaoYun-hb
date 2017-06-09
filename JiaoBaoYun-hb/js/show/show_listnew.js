0b9fe6dc7c70d1c57a3a62ef3bd8fe5d7b2a0ded
rockan007 <rockan007@gmail.com>
2017-06-09 09:31:02 +0800

    去除阴影效果

--- JiaoBaoYun-hb/js/show/show_listnew.js
+++ JiaoBaoYun-hb/js/show/show_listnew.js
@@ -162,15 +162,15 @@ var show_listnew = (function(mod) {
 	}
 	mod.setShowList = function(showCity, listContainer, showData) {
 		var div = document.createElement("div");
-		div.className = "cityNews-container mui-card";
-		var listDiv = document.createElement("div");
-		listDiv.className = "mui-table-view";
+		div.className = "cityNews-container";
+//		var listDiv = document.createElement("div");
+//		listDiv.className = "mui-table-view";
 		for(var i in showData) {
-			var subDiv = document.createElement("li");
+			var subDiv = document.createElement("div");
 			subDiv.className = "mui-table-view-cell news-container";
 			subDiv.innerHTML = mod.getShowInner(showData[i]);
-			listDiv.appendChild(subDiv);
-			div.appendChild(listDiv);
+//			listDiv.appendChild(subDiv);
+			div.appendChild(subDiv);
 			subDiv.info = showData[i];
 		}
 		listContainer.appendChild(div);
