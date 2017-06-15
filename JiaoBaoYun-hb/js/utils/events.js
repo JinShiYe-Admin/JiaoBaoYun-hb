3a26ae4b78b488dd91d4bc05ace665ada1b9924c
BugHunter1234 <444811716@qq.com>
2017-06-15 14:14:28 +0800

    科教界面开启硬件加速

--- JiaoBaoYun-hb/js/utils/events.js
+++ JiaoBaoYun-hb/js/utils/events.js
@@ -673,7 +673,7 @@ var events = (function(mod) {
 			//1.求知问题详情页
 			//2.微课节次详情页
 			//3.微课节次单个详情页
-			if(id == "qiuzhi-question.html" || id == "course_details.html" || id == "course_section.html" || id == 'space-detail.html' || id == 'zonep_sub.html') {
+			if(id == "qiuzhi-question.html" || id == "course_details.html" || id == "course_section.html" || id == 'space-detail.html' || id == 'zonep_sub.html' || id == "sciedu_show_main.html") {
 				styles.hardwareAccelerated = true;
 			}
 			if(id == "show-home1.html") {
