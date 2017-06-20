bb59c7f93a7cd45c2364fbb9809a05f4643e4262
rockan007 <rockan007@gmail.com>
2017-06-20 10:19:36 +0800

    本地提交

--- JiaoBaoYun-hb/js/cloud/upload-filter.js
+++ JiaoBaoYun-hb/js/cloud/upload-filter.js
@@ -1,72 +1,73 @@
 /**
  * 
  */
-var upload_filter = (function(mod) {
-	mod.filterOptions = [{
-		icon: "",
-		title: "上传图片",
-		func: ,
-		type: 2 //2为android&&ios
-	}, {
-		icon: "",
-		title: "上传视频",
-		func: ,
-		type: 2
-	}, {
-		icon: "",
-		title: "本地上传",
-		func: ,
-		type: 1 //1为安卓
-	}]
-	/**
-	 * 
-	 */
-	mod.createFragment = function() {
-		var fragment = document.createDocumentFragment();
-		var div = document.createElement("div");
-		div.className="";
-		for(var i in mod.filterOptions) {
-			var filterOption = mod.filterOptions[i];
-			var subDiv=document.createElement("div");
-			subDiv.className="";
-			subDiv.innerHTML=mod.getInnerByType(filterOption);
-			div.appendChild(subDiv);
+var filter = new Vue({
+	el: "#filter",
+	data: {
+		options: [{
+			icon: "../../image/cloud/up_image.png",
+			title: "上传图片",
+			func: camero.getPic,
+			type: 2 //2为android&&ios
+		}, {
+			icon: "../../image/cloud/up_video.png",
+			title: "上传视频",
+			func: video.recordVideo,
+			type: 2
+		}, {
+			icon: "../../image/cloud/up_local.png",
+			title: "本地上传",
+			func: ,
+			type: 1 //1为安卓
+		}],
+		isShow: false,
+		filterContainer:{
+			position:"fixed",
+			top:"0px",
+			bottom:"0px",
+			left:"0px",
+			width:"100%"
 		}
-		
-	}
-	/**
-	 * 
-	 * @param {Object} option
-	 */
-	mod.getInnerByType = function(option) {
-		switch(option.type) {
-			case 0:
-				break;
-			case 1:
-				break;
-			case 2:
-				break
-			default:
-				break;
+	},
+	methods: {
+		setIsShow: function(isShow) {
+			this.isShow = isShow;
+		},
+		setOptions: function(optionArr) {
+			this.options = optionArr;
+		},
+		setData: function() {
+			var body = document.body;
+			var div=document.createElement("div")
+			div.id="filter";
+			div.setAttribute("v-bind:style",this.filterContainer);
+			div.setAttribute("v-if", this.isShow);
+			var ul = document.createElement("ul");
+			ul.innerHTML = '<li v-for="option of options" v-if="cellIsShow(option)" v-on:click="option:func">' +
+				'<img src="option.icon"/><p>option.title</p>' +
+				'</li>';
+			body.appendChild(ul);
+		},
+		cellIsShow: function(cell) {
+			switch(cell.type) {
+				case 0:
+					if(plus.os.name == "iOS") {
+						return true;
+					}
+					return false;
+				case 1:
+					if(plus.os.name == "Android") {
+						return true;
+					}
+					return false;
+				case 2:
+					if(plus.os.name == "iOS" || plus.os.name == "Android") {
+						return true;
+					}
+					return false;
+				default:
+					return false;
+			}
 		}
 	}
-	/**
-	 * 显示
-	 */
-	mod.show=function(){
-		
-	}
-	/**
-	 * 隐藏
-	 */
-	mod.hide=function(){
-		
-	}
-	/**
-	 * 设置监听
-	 */
-	mod.setListener=function(){
-		
-	}
-	return mod;
-})(upload_filter || {});
+})
